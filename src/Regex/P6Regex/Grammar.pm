93f11bb44e24f3a46057f121789f540028988293
pmichaud <pmichaud@pobox.com>
2009-10-09 03:26:31 -0500

    Convert := operators to = in P6Regex/Grammar.pm .

--- src/Regex/P6Regex/Grammar.pm
+++ src/Regex/P6Regex/Grammar.pm
@@ -40,21 +40,21 @@ grammar Regex::P6Regex::Grammar is PCT::Grammar;
     }
 
     # proto token quantifier { <...> }
-    token quantifier:sym<*> { $<sym>:=['*'] <quantmod> {*} }
-    token quantifier:sym<+> { $<sym>:=['+'] <quantmod> {*} }
-    token quantifier:sym<?> { $<sym>:=['?'] <quantmod> {*} }
+    token quantifier:sym<*> { $<sym>=['*'] <quantmod> {*} }
+    token quantifier:sym<+> { $<sym>=['+'] <quantmod> {*} }
+    token quantifier:sym<?> { $<sym>=['?'] <quantmod> {*} }
 
     token quantmod { ':'? [ '?' | '!' | '+' ]? {*} }
 
     # proto token metachar { <...> }
     token metachar:sym<[ ]> { '[' <nibbler> ']' {*} }
-    token metachar:sym<.> { $<sym>:=['.'] {*} }
-    token metachar:sym<^> { $<sym>:=['^'] {*} }
-    token metachar:sym<^^> { $<sym>:=['^^'] {*} }
-    token metachar:sym<$> { $<sym>:=['$'] {*} }
-    token metachar:sym<$$> { $<sym>:=['$$'] {*} }
-    token metachar:sym<lwb> { $<sym>:=['<<'|'«'] {*} }
-    token metachar:sym<rwb> { $<sym>:=['>>'|'»'] {*} }
+    token metachar:sym<.> { $<sym>=['.'] {*} }
+    token metachar:sym<^> { $<sym>=['^'] {*} }
+    token metachar:sym<^^> { $<sym>=['^^'] {*} }
+    token metachar:sym<$> { $<sym>=['$'] {*} }
+    token metachar:sym<$$> { $<sym>=['$$'] {*} }
+    token metachar:sym<lwb> { $<sym>=['<<'|'«'] {*} }
+    token metachar:sym<rwb> { $<sym>=['>>'|'»'] {*} }
     token metachar:sym<bs> { \\ <backslash> {*} }
     token metachar:sym<assert> { 
         '<' <assertion> 
@@ -63,14 +63,14 @@ grammar Regex::P6Regex::Grammar is PCT::Grammar;
     }
 
     # proto token backslash { <...> }
-    token backslash:sym<w> { $<sym>:=[<[dswnDSWN]>] {*} }
-    token backslash:sym<b> { $<sym>:=[<[bB]>] {*} }
-    token backslash:sym<e> { $<sym>:=[<[eE]>] {*} }
-    token backslash:sym<f> { $<sym>:=[<[fF]>] {*} }
-    token backslash:sym<h> { $<sym>:=[<[hH]>] {*} }
-    token backslash:sym<r> { $<sym>:=[<[rR]>] {*} }
-    token backslash:sym<t> { $<sym>:=[<[tT]>] {*} }
-    token backslash:sym<v> { $<sym>:=[<[vV]>] {*} }
+    token backslash:sym<w> { $<sym>=[<[dswnDSWN]>] {*} }
+    token backslash:sym<b> { $<sym>=[<[bB]>] {*} }
+    token backslash:sym<e> { $<sym>=[<[eE]>] {*} }
+    token backslash:sym<f> { $<sym>=[<[fF]>] {*} }
+    token backslash:sym<h> { $<sym>=[<[hH]>] {*} }
+    token backslash:sym<r> { $<sym>=[<[rR]>] {*} }
+    token backslash:sym<t> { $<sym>=[<[tT]>] {*} }
+    token backslash:sym<v> { $<sym>=[<[vV]>] {*} }
     token backslash:sym<A> { 'A' <.obs: '\\A as beginning-of-string matcher;^'> }
     token backslash:sym<z> { 'z' <.obs: '\\z as end-of-string matcher;$'> }
     token backslash:sym<Z> { 'Z' <.obs: '\\Z as end-of-string matcher;\\n?$'> }
