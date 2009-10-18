73205a09175ac736469bea563d93cec9576af710
pmichaud <pmichaud@pobox.com>
2009-10-17 23:45:34 -0500

    Regexes no longer need explicit action markers at the end.

--- src/Regex/P6Grammar/Grammar.pm
+++ src/Regex/P6Grammar/Grammar.pm
@@ -4,25 +4,22 @@ grammar Regex::P6Grammar::Grammar is Regex::P6Regex::Grammar;
         <grammar_stmt> 
         <regex_stmt>* 
         [ $ || <.panic: 'Confused'> ]
-        {*}
     }
 
-    token name { <.ident> ** '::' {*} }
+    token name { <.ident> ** '::' }
 
     token longname {
         [
         | <.ident> [ ':sym<' <-[>]>* '>' ]?
         ]
-        {*}
     }
 
-    rule grammar_stmt { grammar <name> [ is <base=name> ]? ';' {*} }
+    rule grammar_stmt { grammar <name> [ is <base=name> ]? ';' }
 
     rule regex_stmt {
         $<sym>=[regex|token|rule]
         <longname>
         {*} #= open
         '{'<nibbler>'}'
-        {*}
     }
 
