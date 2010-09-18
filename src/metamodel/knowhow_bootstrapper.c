12bdd7424ec7e76e6e3678f664712d57bbc036d1
Jonathan Worthington <jnthn@jnthn.net>
2010-09-19 00:30:01 +0200

    Fill out a bit more of the KnowHOW bootstrap by getting method dispatch to bottom out. Of course, we can't add and dispatch methods just yet.

--- src/metamodel/knowhow_bootstrapper.c
+++ src/metamodel/knowhow_bootstrapper.c
@@ -9,6 +9,17 @@
 #include "repr_registry.h"
 #include "reprs/KnowHOWREPR.h"
 
+/* This is the find_method where things eventually bottom out. */
+static PMC * bottom_find_method(PARROT_INTERP, PMC *obj, STRING *name, INTVAL hint) {
+    PMC *methods = ((KnowHOWREPRInstance *)PMC_data(obj))->methods;
+    PMC *method  = VTABLE_get_pmc_keyed_str(interp, methods, name);
+    if (PMC_IS_NULL(method))
+        /* XXX Awesomeize. */
+        Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_INVALID_OPERATION,
+                "Could not find method '%Ss'", name);
+    return method;
+}
+
 /* Bootstraps the KnowHOW. This is were things "bottom out" in the meta-model
  * so it's a tad loopy. Basically, we create a KnowHOW type object. We then
  * create an instance from that and add a bunch of methods to it. */
@@ -24,15 +35,22 @@ void RakudoObject_bootstrap_knowhow(PARROT_INTERP) {
     PMC *knowhow_how_pmc = REPR_STRUCT(REPR)->instance_of(interp, REPR, knowhow_pmc);
     KnowHOWREPRInstance *knowhow_how = (KnowHOWREPRInstance *)PMC_data(knowhow_how_pmc);
 
-    /* Set this as the KnowHOW's HOW. */
-    STABLE(knowhow_pmc)->HOW = knowhow_how_pmc;
+    /* Need to give the knowhow_how a twiddled STable with a different
+     * dispatcher, so things bottom out. */
+    PMC *st_copy = create_stable(interp, REPR, knowhow_how_pmc);
+    STABLE_STRUCT(st_copy)->WHAT = knowhow_pmc;
+    STABLE_STRUCT(st_copy)->find_method = bottom_find_method;
+    knowhow_how->common.stable = st_copy;
 
-    /* Add various methods to the KnowHOW. */
+    /* Add various methods to the KnowHOW's HOW. */
     /* XXX TODO */
 
+    /* Set this built up HOW as the KnowHOW's HOW. */
+    STABLE(knowhow_pmc)->HOW = knowhow_how_pmc;
 
-    /* Install it into the root Parrot namespace, since that's where NQP lives.
-     * XXX We'll maybe want to change this in the future. */
+    /* Install KnowHOW into the root Parrot namespace, since that's where NQP
+     * lives.
+     * XXX We'll likely want to change this in the future. */
     Parrot_ns_set_global(interp, Parrot_get_ctx_HLL_namespace(interp),
         Parrot_str_new_constant(interp, "KnowHOW"),
         knowhow_pmc);
