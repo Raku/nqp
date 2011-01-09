a511a302cba91e9533abc5dc74c122797a484a24
Jonathan Worthington <jnthn@jnthn.net>
2011-01-09 15:13:19 +0100

    Bring enough of PCT::HLLCompiler into HLL::Compiler that we can now run and get a prompt. Doesn't yet compile anything, but a step closer. Note that mostly this just wraps PIR bodies into an NQP wrapper; some would be very LHF to translate to NQP fully.

--- src/HLL/Compiler.pm
+++ src/HLL/Compiler.pm
@@ -230,6 +230,17 @@ class HLL::Compiler {
             };
         $*CTXSAVE := 0;
     }
+
+    method panic(*@args) {
+        pir::die(pir::join('', @args))
+    }
+
+    method stages(@value?) {
+        if pir::defined(@value) {
+            @!stages := @value;
+        }
+        @!stages;
+    }
     
     method parsegrammar($value?) {
         if pir::defined($value) {
@@ -243,6 +254,491 @@ class HLL::Compiler {
             $!parseactions := $value;
         }
         $!parseactions;
+    }
+    
+    method astgrammar($value?) {
+        if pir::defined($value) {
+            $!astgrammar := $value;
+        }
+        $!astgrammar;
+    }
+    
+    method commandline_banner($value?) {
+        if pir::defined($value) {
+            $!commandline_banner := $value;
+        }
+        $!commandline_banner;
+    }
+    
+    method commandline_prompt($value?) {
+        if pir::defined($value) {
+            $!commandline_prompt := $value;
+        }
+        $!commandline_prompt;
+    }
+    
+    method compiler_progname($value?) {
+        if pir::defined($value) {
+            $!compiler_progname := $value;
+        }
+        $!compiler_progname;
+    }
+
+    method command_line(@args, *%adverbs) {
+        Q:PIR {
+            .include 'except_severity.pasm'
+            .local pmc args, adverbs
+            args = find_lex '@args'
+            adverbs = find_lex '%adverbs'
+
+            ## this bizarre piece of code causes the compiler to
+            ## immediately abort if it looks like it's being run
+            ## from Perl's Test::Harness.  (Test::Harness versions 2.64
+            ## and earlier have a hardwired commandline option that is
+            ## always passed to an initial run of the interpreter binary,
+            ## whether you want it or not.)  We expect to remove this
+            ## check eventually (or make it a lot smarter than it is here).
+            $S0 = args[2]
+            $I0 = index $S0, '@INC'
+            if $I0 < 0 goto not_harness
+            exit 0
+          not_harness:
+
+            load_bytecode 'dumper.pbc'
+            load_bytecode 'PGE/Dumper.pbc'
+
+            ##  get the name of the program
+            .local string arg0
+            arg0 = args[0]
+
+            ##   perform option processing of command-line args
+            .local pmc opts
+            opts = self.'process_args'(args)
+
+            ##   merge command-line args with defaults passed in from caller
+            .local pmc it
+            it = iter opts
+          mergeopts_loop:
+            unless it goto mergeopts_end
+            $S0 = shift it
+            $P0 = opts[$S0]
+            adverbs[$S0] = $P0
+            goto mergeopts_loop
+          mergeopts_end:
+
+            $I0 = adverbs['help']
+            if $I0 goto usage
+
+            $I0 = adverbs['version']
+            if $I0 goto version
+
+            .local int can_backtrace
+            can_backtrace = can self, 'backtrace'
+            unless can_backtrace goto no_push_eh
+            push_eh uncaught_exception
+          no_push_eh:
+
+            $S0 = adverbs['e']
+            $I0 = exists adverbs['e']
+            if $I0 goto eval_line
+            .local pmc result
+            result = box ''
+            unless args goto interactive
+            $I0 = adverbs['combine']
+            if $I0 goto combine
+            $S0 = args[0]
+            result = self.'evalfiles'($S0, args :flat, adverbs :flat :named)
+            goto save_output
+          combine:
+            result = self.'evalfiles'(args, adverbs :flat :named)
+            goto save_output
+          interactive:
+            self.'interactive'(args :flat, adverbs :flat :named)
+            goto save_output
+          eval_line:
+            result = self.'eval'($S0, '-e', args :flat, adverbs :flat :named)
+
+          save_output:
+            unless can_backtrace goto no_pop_eh
+            pop_eh
+          no_pop_eh:
+            if null result goto end
+            $I0 = defined result
+            unless $I0 goto end
+            .local string target
+            target = adverbs['target']
+            target = downcase target
+            if target != 'pir' goto end
+            .local string output
+            .local pmc ofh
+            $P0 = getinterp
+            ofh = $P0.'stdout_handle'()
+            output = adverbs['output']
+            if output == '' goto save_output_1
+            if output == '-' goto save_output_1
+            ofh = new ['FileHandle']
+            ofh.'open'(output, 'w')
+            unless ofh goto err_output
+          save_output_1:
+            print ofh, result
+            ofh.'close'()
+          end:
+            .return ()
+
+          err_output:
+            .tailcall self.'panic'('Error: file cannot be written: ', output)
+          usage:
+            self.'usage'(arg0)
+            goto end
+          version:
+            self.'version'()
+            goto end
+
+            # If we get an uncaught exception in the program and the HLL provides
+            # a backtrace method, we end up here. We pass it the exception object
+            # so it can render a backtrace, unless the severity is exit or warning
+            # in which case it needs special handling.
+          uncaught_exception:
+            .get_results ($P0)
+            pop_eh
+            $P1 = getinterp
+            $P1 = $P1.'stderr_handle'()
+            $I0 = $P0['severity']
+            if $I0 == .EXCEPT_EXIT goto do_exit
+            $S0 = self.'backtrace'($P0)
+            print $P1, $S0
+            if $I0 <= .EXCEPT_WARNING goto do_warning
+            exit 1
+          do_exit:
+            $I0 = $P0['exit_code']
+            exit $I0
+          do_warning:
+            $P0 = $P0["resume"]
+            push_eh uncaught_exception # Otherwise we get errors about no handler to delete
+            $P0()
+        };
+    }
+
+    method process_args(@args) {
+        Q:PIR {
+            load_bytecode 'Getopt/Obj.pbc'
+
+            .local pmc args
+            args = find_lex '@args'
+            .local string arg0
+            arg0 = shift args
+            self.'compiler_progname'(arg0)
+
+            .local pmc getopts
+            getopts = new ['Getopt';'Obj']
+            getopts.'notOptStop'(1)
+            $P0 = getattribute self, '@!cmdoptions'
+            .local pmc it
+            it = iter $P0
+          getopts_loop:
+            unless it goto getopts_end
+            $S0 = shift it
+            push getopts, $S0
+            goto getopts_loop
+          getopts_end:
+            .tailcall getopts.'get_options'(args)
+        };
+    }
+
+    method evalfiles(@files, *@args, *%adverbs) {
+        Q:PIR {
+            .local pmc files, args, adverbs
+            files = find_lex '@files'
+            args = find_lex '@args'
+            adverbs = find_lex '%adverbs'
+
+            unless null adverbs goto have_adverbs
+            adverbs = new 'Hash'
+          have_adverbs:
+            .local string target
+            target = adverbs['target']
+            target = downcase target
+            .local string encoding
+            encoding = adverbs['encoding']
+            $I0 = does files, 'array'
+            if $I0 goto have_files_array
+            $P0 = new 'ResizablePMCArray'
+            push $P0, files
+            files = $P0
+          have_files_array:
+            .local string code
+            code = ''
+            .local pmc it
+            it = iter files
+          iter_loop:
+            unless it goto iter_end
+            .local string iname
+            .local pmc ifh
+            iname = shift it
+            ifh = new 'FileHandle'
+            unless encoding == 'utf8' goto iter_loop_1
+            ifh.'encoding'(encoding)
+          iter_loop_1:
+            $S0 = ifh.'readall'(iname)
+            code = concat code, $S0
+            ifh.'close'()
+            goto iter_loop
+          iter_end:
+            $S0 = join ' ', files
+            $P1 = box $S0
+            .lex '$?FILES', $P1
+            $P0 = self.'eval'(code, args :flat, adverbs :flat :named)
+            if target == '' goto end
+            if target == 'pir' goto end
+            self.'dumper'($P0, target, adverbs :flat :named)
+          end:
+            .return ($P0)
+
+          err_infile:
+            .tailcall self.'panic'('Error: file cannot be read: ', iname)
+        };
+    }
+
+    method compile($source, *%adverbs) {
+        Q:PIR {
+            .local pmc source, adverbs
+            source = find_lex '$source'
+            adverbs = find_lex '%adverbs'
+
+            .local pmc compiling, options
+            compiling = new ['Hash']
+            .lex '%*COMPILING', compiling
+            compiling['%?OPTIONS'] = adverbs
+
+            .local string target
+            target = adverbs['target']
+            target = downcase target
+
+            .local int stagestats
+            stagestats = adverbs['stagestats']
+
+            .local pmc stages, result, it
+            result = source
+            stages = self.'stages'()
+            it = iter stages
+            if stagestats goto stagestats_loop
+
+          iter_loop:
+            unless it goto have_result
+            .local string stagename
+            stagename = shift it
+            result = self.stagename(result, adverbs :flat :named)
+            if target == stagename goto have_result
+            goto iter_loop
+
+          stagestats_loop:
+            unless it goto have_result
+            stagename = shift it
+            $N0 = time
+            result = self.stagename(result, adverbs :flat :named)
+            $N1 = time
+            $N2 = $N1 - $N0
+            $P0 = getinterp
+            $P1 = $P0.'stderr_handle'()
+            $P1.'print'("Stage '")
+            $P1.'print'(stagename)
+            $P1.'print'("': ")
+            $P2 = new ['ResizablePMCArray']
+            push $P2, $N2
+            $S0 = sprintf "%.3f", $P2
+            $P1.'print'($S0)
+            $P1.'print'(" sec\n")
+            if target == stagename goto have_result
+            goto stagestats_loop
+
+          have_result:
+            .return (result)
+        };
+    }
+
+    method parse($source, *%adverbs) {
+        Q:PIR {
+            .local pmc source, adverbs, parsegrammar, top
+            source = find_lex '$source'
+            adverbs = find_lex '%adverbs'
+
+            .local string tcode
+            tcode = adverbs['transcode']
+            unless tcode goto transcode_done
+            .local pmc tcode_it
+            $P0 = split ' ', tcode
+            tcode_it = iter $P0
+          tcode_loop:
+            unless tcode_it goto transcode_done
+            tcode = shift tcode_it
+            push_eh tcode_fail
+            $I0 = find_encoding tcode
+            $S0 = source
+            $S0 = trans_encoding $S0, $I0
+            assign source, $S0
+            pop_eh
+            goto transcode_done
+          tcode_fail:
+            pop_eh
+            goto tcode_loop
+          transcode_done:
+
+            .local string target
+            target = adverbs['target']
+            target = downcase target
+
+            parsegrammar = self.'parsegrammar'()
+            $I0 = can parsegrammar, 'TOP'
+            unless $I0 goto parsegrammar_string
+            top = find_method parsegrammar, 'TOP'
+            goto have_top
+          parsegrammar_string:
+            $S0 = typeof parsegrammar
+            eq $S0, 'NameSpace', parsegrammar_ns
+            $P0 = self.'parse_name'(parsegrammar)
+            $S0 = pop $P0
+            $P1 = get_hll_global $P0, $S0
+            $I0 = can $P1, 'TOP'
+            unless $I0 goto parsegrammar_ns_string
+            top = find_method $P1, 'TOP'
+            goto have_top
+          parsegrammar_ns_string:
+            $P0 = self.'parse_name'(parsegrammar)
+            top = get_hll_global $P0, 'TOP'
+            unless null top goto have_top
+            goto err_notop
+          parsegrammar_ns:
+            top = parsegrammar['TOP']
+            unless null top goto have_top
+          err_notop:
+            self.'panic'('Cannot find TOP regex in ', parsegrammar)
+          have_top:
+            .local pmc parseactions, action
+            null action
+            if target == 'parse' goto have_action
+            parseactions = self.'parseactions'()
+            $I0 = isa parseactions, ['Undef']
+            if $I0 goto have_action
+            ##  if parseactions is a protoobject, use it directly
+            $I0 = isa parseactions, 'P6protoobject'
+            if $I0 goto action_exact
+            ##  if parseactions is a Class or array, make action directly from that
+            $I0 = isa parseactions, 'Class'
+            if $I0 goto action_make
+            $I0 = isa parseactions, 'NameSpace'
+            if $I0 goto action_namespace
+            $I0 = does parseactions, 'array'
+            if $I0 goto action_make
+            ##  if parseactions is not a String, use it directly.
+            $I0 = isa parseactions, 'String'
+            if $I0 goto action_string
+          action_exact:
+            action = parseactions
+            goto have_action
+          action_namespace:
+            $P0 = get_class parseactions
+            action = new $P0
+            goto have_action
+          action_string:
+            ##  Try the string itself, if that fails try splitting on '::'
+            $P0 = get_class parseactions
+            unless null $P0 goto action_make
+            $S0 = parseactions
+            parseactions = split '::', $S0
+            push_eh err_bad_parseactions
+            $P0 = get_class parseactions
+            if null $P0 goto err_bad_parseactions
+            pop_eh
+          action_make:
+            action = new parseactions
+          have_action:
+            .local pmc match
+            match = top(source, 'grammar' => parsegrammar, 'action' => action)
+            unless match goto err_failedparse
+            .return (match)
+
+          err_no_parsegrammar:
+            self.'panic'('Missing parsegrammar in compiler')
+            .return ()
+          err_failedparse:
+            self.'panic'('Failed to parse source')
+            .return ()
+          err_bad_parseactions:
+            pop_eh
+            $P0 = self.'parseactions'()
+            self.'panic'('Unable to find action grammar ', $P0)
+            .return ()
+        };
+    }
+
+    method past($source, *%adverbs) {
+        Q:PIR {
+            .local pmc source, adverbs
+            source = find_lex '$source'
+            adverbs = find_lex '%adverbs'
+
+          compile_astgrammar:
+            .local pmc astgrammar_name
+            astgrammar_name = self.'astgrammar'()
+            $S0 = typeof astgrammar_name
+            eq $S0, 'NameSpace', astgrammar_ns
+            unless astgrammar_name goto compile_match
+
+            .local pmc astgrammar_namelist
+            .local pmc astgrammar, astbuilder
+            astgrammar_namelist = self.'parse_name'(astgrammar_name)
+            unless astgrammar_namelist goto err_past
+            astgrammar = new astgrammar_namelist
+            astbuilder = astgrammar.'apply'(source)
+            .tailcall astbuilder.'get'('past')
+          astgrammar_ns:
+            $P0 = get_class astgrammar_name
+            astgrammar = new $P0
+            astbuilder = astgrammar.'apply'(source)
+            .tailcall astbuilder.'get'('past')
+
+          compile_match:
+            .local pmc ast
+            ast = source.'ast'()
+            $I0 = isa ast, ['PAST';'Node']
+            unless $I0 goto err_past
+            .return (ast)
+
+          err_past:
+            $S0 = typeof source
+            .tailcall self.'panic'('Unable to obtain PAST from ', $S0)
+        };
+    }
+
+    method post($source, *%adverbs) {
+        Q:PIR {
+            .local pmc source, adverbs
+            source = find_lex '$source'
+            adverbs = find_lex '%adverbs'
+            $P0 = compreg 'PAST'
+            .tailcall $P0.'to_post'(source, adverbs :flat :named)
+        };
+    }
+
+    method pir($source, *%adverbs) {
+        Q:PIR {
+            .local pmc source, adverbs
+            source = find_lex '$source'
+            adverbs = find_lex '%adverbs'
+            $P0 = compreg 'POST'
+            .tailcall $P0.'to_pir'(source, adverbs :flat :named)
+        };
+    }
+
+    method evalpmc($source, *%adverbs) {
+        Q:PIR {
+            .local pmc source, adverbs
+            source = find_lex '$source'
+            adverbs = find_lex '%adverbs'
+            $P0 = compreg 'PIR'
+            $P1 = $P0(source)
+            .return($P1)
+        }
     }
 
 }
