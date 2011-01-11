80c8322b2b886f60ce6024ba5492018bd8abda55
Jonathan Worthington <jnthn@jnthn.net>
2011-01-12 00:52:23 +0100

    A cheating NQPMu.ACCEPTS. Need multi-dispatch done to do the non-cheating version that we have in nqpclr. It's enough to reclaim a regressed test file, for now.

--- src/metamodel/how/NQPMu.pm
+++ src/metamodel/how/NQPMu.pm
@@ -2,7 +2,13 @@ class NQPMu {
     method CREATE() {
         pir::repr_instance_of__PP(self)
     }
+    
     method new() {
         self.CREATE()
+    }
+
+    # XXX This should have siggy (self:U: $topic) and be multi.
+    method ACCEPTS($topic) {
+        pir::type_check__IPP($topic, self.WHAT)
     }
 }
