ab3a2a53be1df138e075a950ef9504f21bba1d51
Jonathan Worthington <jnthn@jnthn.net>
2011-01-12 00:10:30 +0100

    Add a barebones NQPMu and make it the default base class for anything declared with NQPClassHOW.

--- src/metamodel/how/NQPClassHOW.pm
+++ src/metamodel/how/NQPClassHOW.pm
@@ -83,6 +83,12 @@ knowhow NQPClassHOW {
     method compose($obj) {
         # XXX roles...
 
+        # If we have no parents and we're not called NQPMu then add NQPMu as
+        # our parent.
+        if +@!parents == 0 && $!name ne 'NQPMu' {
+            self.add_parent($obj, NQPMu)
+        }
+
         # Some things we only do if we weren't already composed once, like building
         # the MRO.
         unless $!composed {
---
+++ src/metamodel/how/NQPMu.pm
@@ -0,0 +1,8 @@
+class_6m NQPMu {
+    method CREATE() {
+        pir::repr_instance_of__PP(self)
+    }
+    method new() {
+        self.CREATE()
+    }
+}
