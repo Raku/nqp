e282329052862fa75b03a7ad12c953df440037a6
Jonathan Worthington <jnthn@jnthn.net>
2011-01-23 14:07:20 +0100

    Get two of the cheats into HLL::Grammar proper. This first one adds .O (just a wrapper around the PIR) and the second translates .panic into NQP.

--- src/HLL/Grammar.pm
+++ src/HLL/Grammar.pm
@@ -1,7 +1,5 @@
 grammar HLL::Grammar;
 
-    # method EXPR is in src/cheats/hll-grammar.pir
-
     token ws { <!ww> [ \s+ | '#' \N* ]* }
 
     token termish {
@@ -123,3 +121,200 @@ grammar HLL::Grammar;
         ]
     }
 
+    # XXX Everything that follows is a "cheat" because it's still partially in
+    # PIR. They used to live in a separate file, but in the 6model transition got
+    # moved here, since it was the easier way.
+
+=begin 
+
+=item O(spec [, save])
+
+This subrule attaches operator precedence information to
+a match object (such as an operator token).  A typical
+invocation for the subrule might be:
+
+    token infix:sym<+> { <sym> <O( q{ %additive, :pirop<add> } )> }
+
+This says to add all of the attribute of the C<%additive> hash
+(described below) and a C<pirop> entry into the match object
+returned by the C<< infix:sym<+> >> token (as the C<O> named
+capture).  Note that this is a alphabetic 'O", not a digit zero.
+
+Currently the C<O> subrule accepts a string argument describing
+the hash to be stored.  (Note the C< q{ ... } > above.  Eventually
+it may be possible to omit the 'q' such that an actual (constant)
+hash constructor is passed as an argument to C<O>.
+
+The hash built via the string argument to C<O> is cached, so that
+subsequent parses of the same token re-use the hash built from
+previous parses of the token, rather than building a new hash
+on each invocation.
+
+The C<save> argument is used to build "hash" aggregates that can
+be referred to by subsequent calls to C<O>.  For example,
+
+    NQP::Grammar.O(':prec<t=>, :assoc<left>', '%additive' );
+
+specifies the values to be associated with later references to
+"%additive".  Eventually it will likely be possible to use true
+hashes from a package namespace, but this works for now.
+
+Currently the only pairs recognized have the form C< :pair >,
+C< :!pair >, and C<< :pair<strval> >>.
+
+=end
+    method O($spec, $save?) {
+        Q:PIR {
+            .local pmc self
+            .local string spec, save
+            .local int has_save
+            self = find_lex 'self'
+            $P0 = find_lex '$spec'
+            spec = $P0
+            has_save = 0
+            $P0 = find_lex '$save'
+            unless $P0 goto no_save
+            save = $P0
+            has_save = 1
+          no_save:
+
+            # First, get the hash cache.  Right now we have one
+            # cache for all grammars; eventually we may need a way to
+            # separate them out by cursor type.
+            .local pmc ohash
+            ohash = get_global '%!ohash'
+            unless null ohash goto have_ohash
+            ohash = new ['Hash']
+            set_global '%!ohash', ohash
+          have_ohash:
+
+            # See if we've already created a Hash for the current
+            # specification string -- if so, use that.
+            .local pmc hash
+            hash = ohash[spec]
+            unless null hash goto hash_done
+
+            # Otherwise, we need to build a new one.
+            hash = new ['Hash']
+            .local int pos, eos
+            pos = 0
+            eos = length spec
+          spec_loop:
+            pos = find_not_cclass .CCLASS_WHITESPACE, spec, pos, eos
+            if pos >= eos goto spec_done
+            $S0 = substr spec, pos, 1
+            if $S0 == ',' goto spec_comma
+            if $S0 == ':' goto spec_pair
+
+            # If whatever we found doesn't start with a colon, treat it
+            # as a lookup of a previously saved hash to be merged in.
+            .local string lookup
+            .local int lpos
+            # Find the first whitespace or comma
+            lpos = find_cclass .CCLASS_WHITESPACE, spec, pos, eos
+            $I0 = index spec, ',', pos
+            if $I0 < 0 goto have_lookup_lpos
+            if $I0 >= lpos goto have_lookup_lpos
+            lpos = $I0
+          have_lookup_lpos:
+            $I0 = lpos - pos
+            lookup = substr spec, pos, $I0
+            .local pmc lhash, lhash_it
+            lhash = ohash[lookup]
+            if null lhash goto err_lookup
+            lhash_it = iter lhash
+          lhash_loop:
+            unless lhash_it goto lhash_done
+            $S0 = shift lhash_it
+            $P0 = lhash[$S0]
+            hash[$S0] = $P0
+            goto lhash_loop
+          lhash_done:
+            pos = lpos
+            goto spec_loop
+
+            # We just ignore commas between elements for now.
+          spec_comma:
+            inc pos
+            goto spec_loop
+
+            # If we see a colon, then we want to parse whatever
+            # comes next like a pair.
+          spec_pair:
+            # eat colon
+            inc pos
+            .local string name
+            .local pmc value
+            value = new ['Boolean']
+
+            # If the pair is of the form :!name, then reverse the value
+            # and skip the colon.
+            $S0 = substr spec, pos, 1
+            $I0 = iseq $S0, '!'
+            pos += $I0
+            $I0 = not $I0
+            value = $I0
+
+            # Get the name of the pair.
+            lpos = find_not_cclass .CCLASS_WORD, spec, pos, eos
+            $I0 = lpos - pos
+            name = substr spec, pos, $I0
+            pos = lpos
+
+            # Look for a <...> that follows.
+            $S0 = substr spec, pos, 1
+            unless $S0 == '<' goto have_value
+            inc pos
+            lpos = index spec, '>', pos
+            $I0 = lpos - pos
+            $S0 = substr spec, pos, $I0
+            value = box $S0
+            pos = lpos + 1
+          have_value:
+            # Done processing the pair, store it in the hash.
+            hash[name] = value
+            goto spec_loop
+          spec_done:
+            # Done processing the spec string, cache the hash for later.
+            ohash[spec] = hash
+          hash_done:
+
+            # If we've been called as a subrule, then build a pass-cursor
+            # to indicate success and set the hash as the subrule's match object.
+            if has_save goto save_hash
+            ($P0, $I0) = self.'!cursor_start'()
+            $P0.'!cursor_pass'($I0, '')
+            setattribute $P0, '$!match', hash
+            .return ($P0)
+
+            # save the hash under a new entry
+          save_hash:
+            ohash[save] = hash
+            .return (self)
+
+          err_lookup:
+            self.'panic'('Unknown operator precedence specification "', lookup, '"')
+        };
+    }
+
+
+=begin
+
+=item panic([args :slurpy])
+
+Throw an exception at the current cursor location.  If the message
+doesn't end with a newline, also output the line number and offset
+of the match.
+
+=end
+
+    method panic(*@args) {
+        my $pos := self.pos();
+        my $target := self.target();
+        @args.push(' at line ');
+        @args.push(HLL::Compiler.lineof($target, $pos) + 1);
+        @args.push(', near "');
+        @args.push(pir::escape__SS(pir::substr($target, $pos, 10)));
+        @args.push('"');
+        pir::die(pir::join('', @args))
+    }
