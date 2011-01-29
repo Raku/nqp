a82ee698ada8c8755b4206b9bc53eedd3530ddba
Moritz Lenz <moritz@faui2k3.org>
2011-01-29 14:29:26 +0100

    port .Str and .isa from 6model to NQPMu

--- src/metamodel/how/NQPMu.pm
+++ src/metamodel/how/NQPMu.pm
@@ -2,13 +2,23 @@ class NQPMu {
     method CREATE() {
         pir::repr_instance_of__PP(self)
     }
-    
+
     method new() {
         self.CREATE()
+    }
+
+    proto method Str() { * }
+    multi method Str(Mu:U $self:) {
+        self.HOW.name(self) ~ '()'
     }
 
     proto method ACCEPTS($topic) { * }
     multi method ACCEPTS(NQPMu:U $self: $topic) {
         pir::type_check__IPP($topic, self.WHAT)
     }
+
+    method isa($type) {
+        self.HOW.isa(self, $type)
+    }
+
 }
