d014f94f99ce8e927674015841b9bcdaed818c57
Jonathan Worthington <jnthn@jnthn.net>
2011-03-15 00:46:35 +0100

    Work around a MAIN bug.

--- src/Regex/P6Regex/Compiler.pm
+++ src/Regex/P6Regex/Compiler.pm
@@ -1,10 +1,12 @@
 class Regex::P6Regex::Compiler is HLL::Compiler {
 }
 
-sub MAIN(@ARGS) {
+sub MAIN(@ARGS?) {
+    if @ARGS {
     my $p6regex := Regex::P6Regex::Compiler.new();
     $p6regex.language('Regex::P6Regex');
     $p6regex.parsegrammar(Regex::P6Regex::Grammar);
     $p6regex.parseactions(Regex::P6Regex::Actions);
     $p6regex.command_line(@ARGS, :encoding('utf8'), :transcode('ascii iso-8859-1'));
+    }
 }
