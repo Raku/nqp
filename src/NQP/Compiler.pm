9179b83bf81b3cdc461a5aaa8076f33720736ca9
Jonathan Worthington <jnthn@jnthn.net>
2011-03-14 01:09:29 +0100

    Final switch over to having everything in Compiler.pm; toss Compiler.pir. For the first time, this means you can now write a compiler totally in NQP, without needing to write any PIR.

--- src/NQP/Compiler.pm
+++ src/NQP/Compiler.pm
@@ -1,2 +1,18 @@
 class NQP::Compiler is HLL::Compiler {
 }
+
+sub MAIN(@ARGS) {
+    # Create and configure compiler object.
+    my $nqpcomp := NQP::Compiler.new();
+    $nqpcomp.language('nqp');
+    $nqpcomp.parsegrammar(NQP::Grammar);
+    $nqpcomp.parseactions(NQP::Actions);
+    
+    # Add extra command line options.
+    my @clo := $nqpcomp.commandline_options();
+    @clo.push('parsetrace');
+    @clo.push('setting=s');
+    
+    # Enter the compiler.
+    $nqpcomp.command_line(@ARGS, :encoding('utf8'), :transcode('ascii iso-8859-1'));
+}
