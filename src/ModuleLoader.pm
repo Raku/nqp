184a976c2edadcc0b2f04bd1ba62e59ad9dcd761
Jonathan Worthington <jnthn@jnthn.net>
2011-04-23 18:41:01 +0200

    The dumbest GLOBAL merger that could possibly work (TM). Seems it actually will handle what NQP needs - or at least, it doesn't complain about anything.

--- src/ModuleLoader.pm
+++ src/ModuleLoader.pm
@@ -10,12 +10,45 @@ knowhow ModuleLoader {
         $*CTXSAVE := 0;
     }
     
-    method load_module($module_name, $cur_GLOBALish?) {
+    method load_module($module_name, $cur_GLOBALish) {
         # Load the module and capture its mainline.
         my $*CTXSAVE := self;
         my $*MAIN_CTX;
         my $path := pir::join('/', pir::split('::', $module_name)) ~ '.pbc';
         pir::load_bytecode($path);
+
+        # Provided we have a mainline and a GLOBALish in it,
+        # do the merge.
+        if pir::defined($*MAIN_CTX) {
+            my $UNIT := pir::getattribute__PPs($*MAIN_CTX, 'lex_pad');
+            unless pir::isnull($UNIT<GLOBALish>) {
+                merge_globals($cur_GLOBALish, $UNIT<GLOBALish>);
+            }
+        }
+    }
+    
+    # XXX This is a really dumb and minimalistic GLOBAL merger.
+    # For a much more complete one, see sorear++'s work in
+    # Niecza. This one will likely evolve towards that, but for 
+    # now I just need something that's just good enough for the
+    # immediate needs of NQP bootstrapping.
+    sub merge_globals($target, $source) {
+        # XXX For now just merge the top level symbols;
+        # if there's a conflict then don't dig any deeper.
+        # Obviously, just a first cut at this. :-)
+        my %known_symbols;
+        for $target.WHO {
+            %known_symbols{$_.key} := 1;
+        }
+        for $source.WHO {
+            my $sym := $_.key;
+            if !%known_symbols{$sym} {
+                ($target.WHO){$sym} := $_.value;
+            }
+            else {
+                pir::die("Merging GLOBAL symbols failed: duplicate definition of symbol $sym");
+            }
+        }
     }
     
     method load_setting($setting_name) {
