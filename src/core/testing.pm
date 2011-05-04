4b653ba01abca4d5f7e9a28dce593aa45829e6c9
Moritz Lenz <moritz@faui2k3.org>
2011-05-04 10:13:22 +0200

    implement todo()

--- src/core/testing.pm
+++ src/core/testing.pm
@@ -1,4 +1,6 @@
-my $test_counter := 0;
+my $test_counter       := 0;
+my $todo_upto_test_num := 0;
+my $todo_reason        := '';
 
 sub plan($quantity) {
     say("1..$quantity");
@@ -6,7 +8,7 @@ sub plan($quantity) {
 
 sub ok($condition, $desc?) {
     $test_counter := $test_counter + 1;
-    
+
     unless $condition {
         print("not ");
     }
@@ -14,12 +16,22 @@ sub ok($condition, $desc?) {
     if $desc {
         print(" - $desc");
     }
+    if $test_counter <= $todo_upto_test_num {
+        print($todo_reason);
+    }
     print("\n");
     
     $condition ?? 1 !! 0
+}
+
+sub todo($reason, $count) {
+    $todo_upto_test_num := $test_counter + $count;
+    $todo_reason        := "# TODO $reason";
 }
 
 sub skip($desc) {
     $test_counter := $test_counter + 1;
     say("ok $test_counter # SKIP $desc\n");
 }
+
+# vim: ft=perl6
