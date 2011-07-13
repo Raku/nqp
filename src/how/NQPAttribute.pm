2c89ff1139275c1827f596f25600142f58412be0
Jonathan Worthington <jnthn@jnthn.net>
2011-07-13 19:11:54 +0200

    Fix .type and disable not-really-right accessor generation for now (regresses a test, but the test is bogus too).

--- src/how/NQPAttribute.pm
+++ src/how/NQPAttribute.pm
@@ -1,17 +1,20 @@
 knowhow NQPAttribute {
     has $!name;
     has $!type;
+    has $!has_type;
     has $!box_target;
 
-    method new(:$name!, :$type, :$box_target) {
+    method new(:$name!, :$box_target, *%extra) {
         my $attr := pir::repr_instance_of__PP(self);
-        $attr.BUILD(:name($name), :type($type), :box_target($box_target));
+        $attr.BUILD(:name($name), |%extra, :box_target($box_target),
+            :has_type(pir::exists(%extra, 'type')));
         $attr
     }
 
-    method BUILD(:$name, :$type, :$box_target) {
+    method BUILD(:$name, :$type, :$has_type, :$box_target) {
         $!name := $name;
         $!type := $type;
+        $!has_type := $has_type;
         $!box_target := $box_target;
     }
 
@@ -20,7 +23,7 @@ knowhow NQPAttribute {
     }
 
     method type() {
-        $!type // pir::null__P()
+        $!has_type ?? $!type !! pir::null__P()
     }
     
     method has_accessor() {
@@ -36,29 +39,29 @@ knowhow NQPAttribute {
     }
 
     method compose($obj) {
-        my $long_name := ~$!name;
-        if self.has_mutator {
-            my $method := pir::substr($long_name, 1);
-            unless has_method($obj, $method, 0) {
-                $obj.HOW.add_method($obj.WHAT, $method, method ($value?) {
-                        pir::setattribute__vppsp(self, $obj.WHAT, $long_name, $value)
-                            if pir::defined($value);
-
-                        pir::getattribute__ppps(self, $obj.WHAT, $long_name);
-                    }
-                );
-            }
-        }
-        else {
-            my $method := pir::substr($long_name, 2);
-            unless has_method($obj, $method, 0) {
-                $obj.HOW.add_method($obj, $method,
-                    method () {
-                        pir::getattribute__PPPs(self, $obj.WHAT, $long_name);
-                    }
-                );
-            }
-        }
+        #my $long_name := ~$!name;
+        #if self.has_mutator {
+        #    my $method := pir::substr($long_name, 1);
+        #    unless has_method($obj, $method, 0) {
+        #        $obj.HOW.add_method($obj.WHAT, $method, method ($value?) {
+        #                pir::setattribute__vppsp(self, $obj.WHAT, $long_name, $value)
+        #                    if pir::defined($value);
+        #                pir::getattribute__ppps(self, $obj.WHAT, $long_name);
+        #            }
+        #        );
+        #    }
+        #}
+        #else {
+        #    my $method := pir::substr($long_name, 2);
+        #    unless has_method($obj, $method, 0) {
+        #        $obj.HOW.add_method($obj, $method,
+        #            method () {
+        #                pir::getattribute__PPPs(self, $obj.WHAT, $long_name);
+        #            }
+        #        );
+        #    }
+        #}
+        $obj
     }
 
     # Hack to check twigil.
