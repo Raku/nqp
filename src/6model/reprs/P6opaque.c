b9cf1cb64afd0245ce095c6a9f0d948a5a809b79
jnthn <jnthn@jnthn.net>
2011-09-15 16:35:44 +0200

    Fix some insanity in the hint_for API.

--- src/6model/reprs/P6opaque.c
+++ src/6model/reprs/P6opaque.c
@@ -585,8 +585,8 @@ static void bind_attribute_str(PARROT_INTERP, PMC *obj, PMC *class_handle, STRIN
 }
 
 /* Gets the hint for the given attribute ID. */
-static INTVAL hint_for(PARROT_INTERP, PMC *class_key, STRING *name) {
-    P6opaqueREPRData *repr_data = (P6opaqueREPRData *)STABLE(class_key)->REPR_data;
+static INTVAL hint_for(PARROT_INTERP, PMC *obj, PMC *class_key, STRING *name) {
+    P6opaqueREPRData *repr_data = (P6opaqueREPRData *)STABLE(obj)->REPR_data;
     INTVAL slot = try_get_slot(interp, repr_data, class_key, name);
     return slot >= 0 ? slot : NO_HINT;
 }
