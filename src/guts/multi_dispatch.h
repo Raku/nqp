e7be6cc27c4c65bfccd106b4a69369255da7c251
jnthn <jnthn@jnthn.net>
2012-02-14 23:37:33 +0100

    Fix typo.

--- src/guts/multi_dispatch.h
+++ src/guts/multi_dispatch.h
@@ -13,7 +13,7 @@ typedef struct {
     PMC    *st;                 /* S-table, though we don't care about that here. */
     PMC    *sc;                 /* Serialization context, though we don't care about that here. */
     PMC    *types;              /* Set of types for arguments. */
-    PMC    *defindnesses;       /* Set of definedness flags for arguments. */
+    PMC    *definednesses;      /* Set of definedness flags for arguments. */
 } NQP_Signature;
 
 PMC *nqp_multi_dispatch(PARROT_INTERP, PMC *dispatcher, PMC *capture);
