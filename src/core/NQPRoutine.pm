1cb59b49ff7463c037017a8ee3b0faa4f43e06e1
jnthn <jnthn@jnthn.net>
2012-02-14 21:27:01 +0100

    Sketch out NQPRoutine and NQPSignature, which we'll migrate towards in place of a couple of PMCs. This will ease serialization, eliminate two PMCs and also eliminate our use of :instanceof. Hopefully...

---
+++ src/core/NQPRoutine.pm
@@ -0,0 +1,11 @@
+my knowhow NQPRoutine {
+    has $!do;
+    has $!signature;
+    has $!dispatchees;
+    has $!dispatch_cache;
+}
+
+my knowhow NQPSignature {
+    has $!types;
+    has $!definednesses;
+}
