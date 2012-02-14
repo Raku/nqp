37c6c88b5226d0d3fa0992b0bb49978ede733ae3
jnthn <jnthn@jnthn.net>
2012-02-14 21:48:26 +0100

    NQPRoutine needs to delegate invocation in order to serve as a code object.

--- src/core/NQPRoutine.pm
+++ src/core/NQPRoutine.pm
@@ -4,6 +4,8 @@ my knowhow NQPRoutine {
     has $!dispatchees;
     has $!dispatch_cache;
 }
+pir::stable_publish_vtable_handler_mapping__vPP(NQPRoutine,
+    nqp::hash('invoke', nqp::list(NQPRoutine, '$!do')));
 
 my knowhow NQPSignature {
     has $!types;
