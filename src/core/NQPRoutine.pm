e3b73bbf3dd43955b31dd13f6c64efdfe201abb0
Jonathan Worthington <jnthn@jnthn.net>
2012-02-16 22:47:51 +0100

    Sketch some dispatcher related methods into NQPRoutine.

--- src/core/NQPRoutine.pm
+++ src/core/NQPRoutine.pm
@@ -3,6 +3,17 @@ my knowhow NQPRoutine {
     has $!signature;
     has $!dispatchees;
     has $!dispatch_cache;
+    method add_dispatchee($code) {
+        $!dispatchees.push($code);
+    }
+    method is_dispatcher() {
+        !nqp::isnull($!dispatchees)
+    }
+    method derive_dispatcher() {
+        my $der := pir::repr_clone__PP(self);
+        nqp::bindattr($der, NQPRoutine, '$!dispatchees', pir::clone__PP($!dispatchees));
+        $der
+    }
 }
 pir::stable_publish_vtable_handler_mapping__vPP(NQPRoutine,
     nqp::hash('invoke', nqp::list(NQPRoutine, '$!do')));
