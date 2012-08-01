ae3c7679840e2d6f46c9452875f4957ab31abdd4
jnthn <jnthn@jnthn.net>
2012-08-01 23:12:26 +0200

    Add a temporary NQPQ to do the initial bits of QAST migration; yes, the missing dependency in the Makefile is deliberate.

---
+++ src/NQPQ/Compiler.pm
@@ -0,0 +1,32 @@
+class NQP::Compiler is HLL::Compiler {
+}
+
+sub MAIN(@ARGS) {
+    # Create and configure compiler object.
+    my $nqpcomp := NQP::Compiler.new();
+    $nqpcomp.language('nqp');
+    $nqpcomp.parsegrammar(NQP::Grammar);
+    $nqpcomp.parseactions(NQP::Actions);
+    hll-config($nqpcomp.config);
+    
+    # Add extra command line options.
+    my @clo := $nqpcomp.commandline_options();
+    @clo.push('parsetrace');
+    @clo.push('setting=s');
+    @clo.push('setting-path=s');
+    @clo.push('module-path=s');
+    @clo.push('vmlibs=s');
+    @clo.push('no-regex-lib');
+    @clo.push('old-regex-lib');
+    
+    # Hack while we still use the old PAST classes to make sure they are
+    # loaded.
+    try {
+        pir::get_hll_global__Ps('ModuleLoader').load_module('PASTRegex', GLOBALish);
+    }
+    
+    # Enter the compiler.
+    $nqpcomp.command_line(@ARGS, :encoding('utf8'), :transcode('ascii iso-8859-1'));
+    # Close event logging
+    $nqpcomp.nqpevent();
+}
