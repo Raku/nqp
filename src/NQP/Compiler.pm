ba3757329cd1738a56a4725b754f298e7669cd02
jnthn <jnthn@jnthn.net>
2012-12-20 21:36:04 +0100

    Make NQP compreg when loaded as a module.

--- src/NQP/Compiler.pm
+++ src/NQP/Compiler.pm
@@ -1,24 +1,24 @@
 class NQP::Compiler is HLL::Compiler {
 }
 
+# Create and configure compiler object.
+my $nqpcomp := NQP::Compiler.new();
+$nqpcomp.language('nqp');
+$nqpcomp.parsegrammar(NQP::Grammar);
+$nqpcomp.parseactions(NQP::Actions);
+hll-config($nqpcomp.config);
+
+# Add extra command line options.
+my @clo := $nqpcomp.commandline_options();
+@clo.push('parsetrace');
+@clo.push('setting=s');
+@clo.push('setting-path=s');
+@clo.push('module-path=s');
+@clo.push('vmlibs=s');
+@clo.push('no-regex-lib');
+@clo.push('old-regex-lib');
+
 sub MAIN(@ARGS) {
-    # Create and configure compiler object.
-    my $nqpcomp := NQP::Compiler.new();
-    $nqpcomp.language('nqp');
-    $nqpcomp.parsegrammar(NQP::Grammar);
-    $nqpcomp.parseactions(NQP::Actions);
-    hll-config($nqpcomp.config);
-    
-    # Add extra command line options.
-    my @clo := $nqpcomp.commandline_options();
-    @clo.push('parsetrace');
-    @clo.push('setting=s');
-    @clo.push('setting-path=s');
-    @clo.push('module-path=s');
-    @clo.push('vmlibs=s');
-    @clo.push('no-regex-lib');
-    @clo.push('old-regex-lib');
-    
     # Enter the compiler.
     $nqpcomp.command_line(@ARGS, :encoding('utf8'), :transcode('ascii iso-8859-1'));
 
