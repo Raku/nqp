e50b1c25fd00f4a5dd3fd43eb3805f8f1e6bbc9e
Arne Skjærholt <arnsholt@gmail.com>
2013-02-01 14:39:13 +0100

    Teach P6num about different sizes.

--- src/6model/reprs/P6num.h
+++ src/6model/reprs/P6num.h
@@ -12,6 +12,11 @@ typedef struct {
     P6numBody body;
 } P6numInstance;
 
+/* The bit width requirement is shared for all instances of the same type. */
+typedef struct {
+    INTVAL bits;
+} P6numREPRData;
+
 /* Initializes the P6num REPR. */
 REPROps * P6num_initialize(PARROT_INTERP);
 
