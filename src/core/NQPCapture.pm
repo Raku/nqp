02e8737661e5288402d865aff20f0aaebeaf720b
jnthn <jnthn@jnthn.net>
2013-02-09 22:42:53 +0100

    Switch NQPCapture to use associative_delegate.

--- src/core/NQPCapture.pm
+++ src/core/NQPCapture.pm
@@ -1,16 +1,6 @@
 my class NQPCapture {
     has @!array is positional_delegate;
-
-    has %!hash
-		is parrot_vtable_handler('get_pmc_keyed_str')
-		is parrot_vtable_handler('get_pmc_keyed')
-		is parrot_vtable_handler('set_pmc_keyed_str')
-		is parrot_vtable_handler('set_pmc_keyed')
-		is parrot_vtable_handler('exists_keyed')
-		is parrot_vtable_handler('exists_keyed_str')
-		is parrot_vtable_handler('delete_keyed')
-		is parrot_vtable_handler('delete_keyed_str')
-		;
+    has %!hash is associative_delegate;
 
     method new() {
         my $n := self.CREATE;
