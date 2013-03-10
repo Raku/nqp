117931a89472fa00043c77fedfc32f328995b45d
Arne Skjærholt <arnsholt@gmail.com>
2013-03-10 21:33:49 +0100

    Add NQPArray type.

---
+++ src/core/NQPArray.pm
@@ -0,0 +1,6 @@
+my class NQPArray is repr('VMArray') {
+    method push($value) { nqp::push(self, $value) }
+    method pop() { nqp::pop(self) }
+    method unshift($value) { nqp::unshift(self, $value) }
+    method shift() { nqp::shift(self) }
+}
