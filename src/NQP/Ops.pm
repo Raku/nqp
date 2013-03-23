22ecde94938f00e8005ffe8e7c814aaaa54c8a31
Arne Skjærholt <arnsholt@gmail.com>
2013-03-23 20:04:03 +0100

    Implement smart numification similar to how it works on JVM.

--- src/NQP/Ops.pm
+++ src/NQP/Ops.pm
@@ -78,9 +78,7 @@ $ops.add_hll_op('nqp', 'postdec', -> $qastcomp, $op {
         )));
 });
 
-$ops.add_hll_op('nqp', 'numify', -> $qastcomp, $op {
-    $qastcomp.as_post($op[0], :want('n'))
-});
+$ops.add_hll_pirop_mapping('nqp', 'numify', 'smart_numify', 'NP');
 
 $ops.add_hll_op('nqp', 'stringify', -> $qastcomp, $op {
     $qastcomp.as_post($op[0], :want('s'))
