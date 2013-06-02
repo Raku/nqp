cd35271c0cb7fb4b2655df5a21443068f210d409
Jonathan Worthington <jnthn@jnthn.net>
2013-06-02 18:37:59 +0200

    Add missing gc_mark registration.

--- src/vm/parrot/6model/reprs/NativeCall.c
+++ src/vm/parrot/6model/reprs/NativeCall.c
@@ -139,6 +139,7 @@ REPROps * NativeCall_initialize(PARROT_INTERP,
     this_repr->allocate = allocate;
     this_repr->initialize = initialize;
     this_repr->copy_to = copy_to;
+    this_repr->gc_mark = gc_mark;
     this_repr->gc_free = gc_free;
     this_repr->gc_cleanup = gc_cleanup;
     this_repr->get_storage_spec = get_storage_spec;
