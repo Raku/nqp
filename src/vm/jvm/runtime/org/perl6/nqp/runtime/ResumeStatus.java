97a0ee6a643eb63ffc9e8a54a194a8079946cd9c
Stefan O'Rear <stefanor@cox.net>
2013-06-10 01:44:00 -0700

    JNQP continuations 1/N: SaveStackException and ResumeStatus as interpreted by the current design

--- src/vm/jvm/runtime/org/perl6/nqp/runtime/ResumeStatus.java
+++ src/vm/jvm/runtime/org/perl6/nqp/runtime/ResumeStatus.java
@@ -1,70 +1,41 @@
 package org.perl6.nqp.runtime;
-import java.util.Stack;
 
-import org.perl6.nqp.sixmodel.SixModelObject;
+import java.lang.invoke.MethodHandle;
 
+/**
+ * Represents a composable continuation.
+ */
 public class ResumeStatus {
-    private class ResumeFrame {
-        public int ResumePoint;
-        public long[] SavedInts;
-        public double[] SavedNums;
-        public String[] SavedStrs;
-        public SixModelObject[] SavedObjs;
+    /**
+     * Represents one saved, not-currently-executing execution frame.
+     */
+    public static class Frame {
+        /** Identifies the method that was executing. */
+        public final MethodHandle method;
+        /** Identifies the call site within the method. */
+        public final int resumePoint;
+        /** Local variables saved for the method. */
+        public final Object[] saveSpace;
+        /** If present, this frame should be set as the running frame on reentry. */
+        public final CallFrame callFrame;
+        /** The next deeper frame. */
+        public final Frame next;
+
+        /** Constructor which sets all fields. */
+        public Frame(MethodHandle method, int resumePoint, Object[] saveSpace, CallFrame callFrame, Frame next) {
+            this.method = method;
+            this.resumePoint = resumePoint;
+            this.saveSpace = saveSpace;
+            this.callFrame = callFrame;
+            this.next = next;
+        }
     }
-    
-    private Stack<ResumeFrame> SavedFrames;
-    private ResumeFrame CurrentFrame;
-    private int CurInt, CurNum, CurStr, CurObj;
-    
-    public ResumeStatus() {
-        SavedFrames = new Stack<ResumeFrame>();
-    }
-    
-    public void PushFrame(int ResumePoint, int NumInts, int NumNums, int NumStrs, int NumObjs) {
-        CurrentFrame = new ResumeFrame();
-        SavedFrames.push(CurrentFrame);
-        CurrentFrame.ResumePoint = ResumePoint;
-        if (NumInts > 0)
-            CurrentFrame.SavedInts = new long[NumInts];
-        if (NumNums > 0)
-            CurrentFrame.SavedNums = new double[NumNums];
-        if (NumStrs > 0)
-            CurrentFrame.SavedStrs = new String[NumStrs];
-        if (NumObjs > 0)
-            CurrentFrame.SavedObjs = new SixModelObject[NumObjs];
-        CurInt = CurNum = CurStr = CurObj = 0;
-    }
-    
-    public void StashInt(long Stashee) {
-        CurrentFrame.SavedInts[CurInt++] = Stashee;
-    }
-    public void StashNum(double Stashee) {
-        CurrentFrame.SavedNums[CurNum++] = Stashee;
-    }
-    public void StashStr(String Stashee) {
-        CurrentFrame.SavedStrs[CurStr++] = Stashee;
-    }
-    public void StashObj(SixModelObject Stashee) {
-        CurrentFrame.SavedObjs[CurObj++] = Stashee;
-    }
-    
-    public int PopFrame()
-    {
-        CurrentFrame = SavedFrames.pop();
-        CurInt = CurNum = CurStr = CurObj = 0;
-        return CurrentFrame.ResumePoint;
-    }
-    
-    public long UnstashInt() {
-        return CurrentFrame.SavedInts[CurInt++];
-    }
-    public double UnstashNum() {
-        return CurrentFrame.SavedNums[CurNum++];
-    }
-    public String UnstashStr() {
-        return CurrentFrame.SavedStrs[CurStr++];
-    }
-    public SixModelObject UnstashObj() {
-        return CurrentFrame.SavedObjs[CurObj++];
+
+    /** The first frame of this continuation.  Subsequent frames can be accessed using {@link Frame#next}. */
+    public Frame top;
+
+    /** Builds a new continuation from the stack stored in a {@link SaveStackException}. */
+    public ResumeStatus(SaveStackException ex) {
+        this.top = ex.top;
     }
 }
--- src/vm/jvm/runtime/org/perl6/nqp/runtime/SaveStackException.java
+++ src/vm/jvm/runtime/org/perl6/nqp/runtime/SaveStackException.java
@@ -1,6 +1,21 @@
 package org.perl6.nqp.runtime;
 
+import java.lang.invoke.MethodHandle;
+
+/** Thrown by dcshift operations to cause all currently executing frames to save their state. */
+
 @SuppressWarnings("serial")
 public class SaveStackException extends Exception {
-    public ResumeStatus rs;
+    /** Topmost frame saved so far. */
+    public ResumeStatus.Frame top;
+    /** Tag identifying a specific instance of reset. */
+    public int tag; /* make a sixmodelobject? */
+
+    public SaveStackException(int tag) {
+        this.tag = tag;
+    }
+
+    public void pushFrame(MethodHandle method, int resumePoint, Object[] saveSpace, CallFrame callFrame) {
+        top = new ResumeStatus.Frame(method, resumePoint, saveSpace, callFrame, top);
+    }
 }
