d0fcc5e1d63ff92bf076968fb43ccc208837c43c
Stefan O'Rear <stefanor@cox.net>
2013-07-06 23:51:01 -0700

    Remove a pointless and warning cast

--- src/vm/jvm/runtime/org/perl6/nqp/runtime/Ops.java
+++ src/vm/jvm/runtime/org/perl6/nqp/runtime/Ops.java
@@ -3641,7 +3641,7 @@ public final class Ops {
             java.lang.management.RuntimeMXBean runtime = java.lang.management.ManagementFactory.getRuntimeMXBean();
             java.lang.reflect.Field jvm = runtime.getClass().getDeclaredField("jvm");
             jvm.setAccessible(true);
-            sun.management.VMManagement mgmt = (sun.management.VMManagement) jvm.get(runtime);
+            Object mgmt = jvm.get(runtime);
             java.lang.reflect.Method pid_method = mgmt.getClass().getDeclaredMethod("getProcessId");
             pid_method.setAccessible(true);
             return (Integer)pid_method.invoke(mgmt);
