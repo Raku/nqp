a6207650863879a7acec3175884f4285897549dd
Donald Hunter <donald@sealgair.com>
2013-07-19 22:50:04 +0100

    Restructured install layout to resemble parrot install layout.

--- tools/build/install-jvm-runner.pl
+++ tools/build/install-jvm-runner.pl
@@ -12,22 +12,25 @@ unless (File::Spec->file_name_is_absolute($prefix)) {
     $prefix = File::Spec->rel2abs($prefix);
 }
 
+my $jar_dir = File::Spec->catfile($prefix, 'languages', 'nqp', 'runtime');
+my $lib_dir = File::Spec->catfile($prefix, 'languages', 'nqp', 'lib');
+
 if ($^O eq 'MSWin32') {
-    my $install_to = File::Spec->catfile($prefix, 'nqp.bat');
+    my $install_to = File::Spec->catfile($prefix, 'bin', 'nqp.bat');
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
-    print $fh '@java -Xbootclasspath/a:' . $prefix . ';' . $prefix . '\\nqp-runtime.jar;' .
-              $prefix . '\\asm-4.1.jar;' . $prefix . "\\jline-1.0.jar;$prefix\\nqp.jar -cp $prefix nqp %*\n";
+    print $fh '@java -Xbootclasspath/a:$jar_dir\\nqp-runtime.jar;' .
+              "$jar_dir\\asm-4.1.jar;$jar_dir\\jline-1.0.jar;$lib_dir\\nqp.jar -cp $lib_dir nqp %*\n";
     close $fh
         or die "Could not close $install_to: $!";
 }
 else {
-    my $install_to = File::Spec->catfile($prefix, 'nqp');
+    my $install_to = File::Spec->catfile($prefix, 'bin', 'nqp');
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
     print $fh "#!/bin/sh\n";
-    print $fh "exec java -Xbootclasspath/a:$prefix:$prefix/nqp-runtime.jar:" .
-              "$prefix/asm-4.1.jar:$prefix/jline-1.0.jar:$prefix/nqp.jar -cp $prefix nqp \"\$\@\"\n";
+    print $fh "exec java -Xbootclasspath/a:$jar_dir/nqp-runtime.jar:" .
+              "$jar_dir/asm-4.1.jar:$jar_dir/jline-1.0.jar:$lib_dir/nqp.jar -cp $lib_dir nqp \"\$\@\"\n";
     close $fh
         or die "Could not close $install_to: $!";
     chmod 0755, $install_to;
