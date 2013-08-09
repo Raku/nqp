b21292790494afcff2d077fc03d29ce2951914d0
jnthn <jnthn@jnthn.net>
2013-08-10 00:49:56 +0200

    Implement asynchronous slurp.

--- src/vm/jvm/runtime/org/perl6/nqp/io/AsyncFileHandle.java
+++ src/vm/jvm/runtime/org/perl6/nqp/io/AsyncFileHandle.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
@@ -13,7 +14,9 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.perl6.nqp.runtime.CallSiteDescriptor;
 import org.perl6.nqp.runtime.ExceptionHandling;
+import org.perl6.nqp.runtime.Ops;
 import org.perl6.nqp.runtime.ThreadContext;
 import org.perl6.nqp.sixmodel.SixModelObject;
 
@@ -59,9 +62,55 @@ public class AsyncFileHandle implements IIOClosable, IIOEncodable, IIOAsyncReada
         dec = cs.newDecoder();
     }
     
-    public void slurp(ThreadContext tc, SixModelObject Str, SixModelObject done,
-                      SixModelObject error) {
-        throw new RuntimeException("Async slurp NYI");
+    private class SlurpState {
+        public ByteBuffer bb;
+        public long expected;
+    }
+    private static final CallSiteDescriptor slurpResultCSD = new CallSiteDescriptor(
+        new byte[] { CallSiteDescriptor.ARG_OBJ }, null);
+    public void slurp(final ThreadContext tc, final SixModelObject Str,
+                      final SixModelObject done, final SixModelObject error) {
+        try {
+            SlurpState ss = new SlurpState();
+            ss.expected = chan.size();
+            ss.bb = ByteBuffer.allocate((int)ss.expected);
+            
+            final CompletionHandler<Integer, SlurpState> ch = new CompletionHandler<Integer, SlurpState>() {
+                public void completed(Integer bytes, SlurpState ss) {
+                    if (ss.bb.position() == ss.expected) {
+                        try {
+                            /* We're done. Decode and box. */
+                            ThreadContext curTC = tc.gc.getCurrentThreadContext();
+                            ss.bb.flip();
+                            String decoded = dec.decode(ss.bb).toString();
+                            SixModelObject boxed = Ops.box_s(decoded, Str, curTC);
+                            
+                            /* Call done handler. */
+                            Ops.invokeDirect(curTC, done, slurpResultCSD, new Object[] { boxed });
+                        } catch (IOException e) {
+                            failed(e, ss);
+                        }
+                    }
+                    else {
+                        /* Need to read some more. */
+                        chan.read(ss.bb, 0, ss, this);
+                    }
+                }
+                
+                public void failed(Throwable exc, SlurpState ss) {
+                    /* Box error. */
+                    ThreadContext curTC = tc.gc.getCurrentThreadContext();
+                    SixModelObject boxed = Ops.box_s(exc.toString(), Str, curTC);
+                    
+                    /* Call error handler. */
+                    Ops.invokeDirect(curTC, error, slurpResultCSD, new Object[] { boxed });
+                }
+            };
+            
+            chan.read(ss.bb, 0, ss, ch);
+        } catch (IOException e) {
+            throw ExceptionHandling.dieInternal(tc, e);
+        }
     }
     
     public void lines(ThreadContext tc, SixModelObject Str, boolean chomp,
