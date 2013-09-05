cc25bbc39dc03cc5b3ab64902d6318b62e911ff1
Arne Skjærholt <arnsholt@gmail.com>
2013-09-05 20:07:51 +0200

    Stub native call ops.

---
+++ src/vm/jvm/runtime/org/perl6/nqp/runtime/NativeCallOps.java
@@ -0,0 +1,17 @@
+package org.perl6.nqp.runtime;
+
+import org.perl6.nqp.sixmodel.SixModelObject;
+
+public final class NativeCallOps {
+    public static long init() {
+        return 1L;
+    }
+
+    public static long build() {
+        return 1L;
+    }
+
+    public static SixModelObject call() {
+        return null;
+    }
+}
