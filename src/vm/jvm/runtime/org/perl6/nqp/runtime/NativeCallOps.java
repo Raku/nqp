a83bf2b5b8257c55230a05301ae89bfccb63b58a
Arne Skjærholt <arnsholt@gmail.com>
2013-09-09 16:41:22 +0200

    Build NativeCallInstance.{arg_types,ret_type} in nqp::buildnativecall.

--- src/vm/jvm/runtime/org/perl6/nqp/runtime/NativeCallOps.java
+++ src/vm/jvm/runtime/org/perl6/nqp/runtime/NativeCallOps.java
@@ -5,6 +5,7 @@ import com.sun.jna.NativeLibrary;
 import org.perl6.nqp.sixmodel.SixModelObject;
 
 import org.perl6.nqp.sixmodel.reprs.NativeCallInstance;
+import org.perl6.nqp.sixmodel.reprs.NativeCallInstance.ArgType;
 
 public final class NativeCallOps {
     public static long init() {
@@ -13,7 +14,7 @@ public final class NativeCallOps {
         return 1L;
     }
 
-    public static long build(SixModelObject target, String libname, String symbol, String convention, SixModelObject arguments, SixModelObject returns) {
+    public static long build(SixModelObject target, String libname, String symbol, String convention, SixModelObject arguments, SixModelObject returns, ThreadContext tc) {
         NativeCallInstance call = getNativeCallInstance(target);
 
         /* Load the library and locate the symbol. */
@@ -24,6 +25,13 @@ public final class NativeCallOps {
         /* TODO: Set the calling convention. */
 
         /* Set up the argument types. */
+        int n = (int) arguments.elems(tc);
+        call.arg_types = new ArgType[n];
+        for (int i = 0; i < n; i++) {
+            call.arg_types[i] = getArgType(tc, arguments.at_pos_boxed(tc, i), false);
+        }
+
+        call.ret_type = getArgType(tc, returns, true);
 
         return 1L;
     }
@@ -42,5 +50,23 @@ public final class NativeCallOps {
             call = null;
         }
         return call;
+    }
+
+    private static ArgType getArgType(ThreadContext tc, SixModelObject info, boolean isReturn) {
+        String type_name = info.at_key_boxed(tc, "type").get_str(tc);
+
+        ArgType type = ArgType.VOID;
+        try {
+            type = ArgType.valueOf(ArgType.class, type_name.toUpperCase());
+        }
+        catch (IllegalArgumentException e) {
+            ExceptionHandling.dieInternal(tc, String.format("Unknown type '%s' used for native call", type_name));
+        }
+
+        if (!isReturn && type == ArgType.VOID) {
+            ExceptionHandling.dieInternal(tc, "Can only use 'void' type on native call return values");
+        }
+
+        return type;
     }
 }
