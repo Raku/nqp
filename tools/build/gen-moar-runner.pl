d24a375fd234e046f7c2620e2a2d221b489976d3
jnthn <jnthn@jnthn.net>
2013-10-11 20:58:47 +0200

    Generate a runner for MoarVM.
    So after the build, ./nqp works. Needs testing on non-Windows.

---
+++ tools/build/gen-moar-runner.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+# Copyright (C) 2013, The Perl Foundation.
+
+use strict;
+use warnings;
+use 5.008;
+use File::Spec;
+
+my ($moar) = @ARGV;
+
+if ($^O eq 'MSWin32') {
+    my $install_to = 'nqp.bat';    
+    open my $fh, ">", $install_to
+        or die "Could not open $install_to: $!";
+    print $fh '@ "' . $moar . '" nqp.moarvm %*' . "\n";
+    close $fh
+        or die "Could not close $install_to: $!";
+}
+else {
+    my $install_to = 'nqp';
+    open my $fh, ">", $install_to
+        or die "Could not open $install_to: $!";
+    print $fh "#!/bin/sh\n";
+    print $fh "exec $moar nqp.moarvm \"\$\@\"\n";
+    close $fh
+        or die "Could not close $install_to: $!";
+    chmod 0755, $install_to;
+}
