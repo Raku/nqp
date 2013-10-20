1a09cc694512259c03b94c6cbafdf534b252d5c1
Moritz Lenz <moritz@faui2k3.org>
2013-10-20 22:27:51 +0200

    Unify Configure.pl and ConfigureMoar.pl
    now "perl Configure.pl --backends=moar" creates a Makefile
    suitable for building nqp on Moar

--- Configure.pl
+++ Configure.pl
@@ -17,7 +17,8 @@ MAIN: {
         unshift @ARGV, shellwords(slurp('config.default'));
     }
 
-    my %config;
+    my $slash  = $^O eq 'MSWin32' ? '\\' : '/';
+    my %config = (perl => $^X);
     $config{'nqp_config_status'} = join(' ', map { "\"$_\""} @ARGV);
 
     my $exe = $NQP::Configure::exe;
@@ -26,6 +27,7 @@ MAIN: {
     GetOptions(\%options, 'help!', 'prefix=s',
                'with-parrot=s', 'gen-parrot:s',
                'make-install!', 'makefile-timing!',
+               'backends=s',
                'parrot-config=s', 'parrot-option=s@');
 
     # Print help if it's requested
@@ -40,9 +42,28 @@ MAIN: {
               "Use --with-parrot to specify the parrot executable to use."
     }
 
+    my $default_backend;
+    my %backends;
+    my %known_backends = (parrot => 1, jvm => 1, moar => 1);
+    if ($options{backends}) {
+        for my $be (split /,/, $options{backends}) {
+            $be = lc $be;
+            unless ($known_backends{$be}) {
+                die "Unknown backend: '$be'; Known backends: " .
+                    join(', ', sort keys %known_backends) . "\n";
+            }
+            $default_backend ||= $be;
+            $backends{$be} = 1;
+        }
+    }
+    else {
+        # TODO: come up with more sensible defaults
+        $backends{parrot} = 1;
+        $default_backend = 'parrot';
+    }
+
     my $prefix      = ($options{'prefix'} && abs_path($options{'prefix'})) || cwd().'/install';
-    my $with_parrot = $options{'with-parrot'};
-    my $gen_parrot  = $options{'gen-parrot'};
+    $config{prefix} = $prefix;
 
     # Save options in config.status
     unlink('config.status');
@@ -51,105 +72,137 @@ MAIN: {
             "$^X Configure.pl $config{'nqp_config_status'} \$*\n";
         close($CONFIG_STATUS);
     }
-
-    my ($par_want) = split(' ', slurp('tools/build/PARROT_REVISION'));
-
-    if (defined $gen_parrot) {
-        $with_parrot = gen_parrot($par_want, %options, prefix => $prefix);
-    }
-
-    my @errors;
-
-    my %par_config;
-    if ($with_parrot) {
-        %par_config = read_parrot_config($with_parrot)
-          or push @errors, "Unable to read configuration from $with_parrot.";
-    }
-    else {
-        %par_config = read_parrot_config("$prefix/bin/parrot$exe", "parrot$exe")
-          or push @errors, "Unable to find parrot.";
-        $with_parrot = fill_template_text('@bindir@/parrot@exe@', %par_config);
-    }
-
-    %config = (%config, %par_config);
-    my $par_have = $config{'parrot::git_describe'} || '';
-    if ($par_have && cmp_rev($par_have, $par_want) < 0) {
-        push @errors, "Parrot revision $par_want required (currently $par_have).";
-    }
-
-    if (!@errors) {
-        push @errors, verify_install([@NQP::Configure::required_parrot_files],
-                                     %config);
-        push @errors,
-          "(Perhaps you need to 'make install', 'make install-dev',",
-          "or install the 'devel' package for Parrot?)"
-          if @errors;
-    }
-
-    if (@errors && !defined $gen_parrot) {
-        push @errors,
-          "\nTo automatically clone (git) and build a copy of Parrot $par_want,",
-          "try re-running Configure.pl with the '--gen-parrot' option.",
-          "Or, use '--with-parrot=' to explicitly specify the Parrot",
-          "executable to use to build NQP.";
-    }
-
-    sorry(@errors) if @errors;
-
-    print "Using $with_parrot (version $config{'parrot::git_describe'}).\n";
-
     $config{'makefile-timing'} = $options{'makefile-timing'};
     $config{'stagestats'} = '--stagestats' if $options{'makefile-timing'};
     $config{'shell'} = $^O eq 'MSWin32' ? 'cmd' : 'sh';
     $config{'bat'}   = $^O eq 'MSWin32' ? '.bat' : '';
-    if ($^O eq 'MSWin32' or $^O eq 'cygwin') {
-        $config{'dll'} = '$(PARROT_BIN_DIR)/$(PARROT_LIB_SHARED)';
-        $config{'dllcopy'} = '$(PARROT_LIB_SHARED)';
-        $config{'make_dllcopy'} =
-            '$(PARROT_DLL_COPY) : $(PARROT_DLL)'."\n\t".'$(CP) $(PARROT_DLL) .';
+    $config{'cpsep'} = $^O eq 'MSWin32' ? ';' : ':';
+    $config{'slash'} = $slash;
+
+    if ($backends{parrot}) {
+        my $with_parrot = $options{'with-parrot'};
+        my $gen_parrot  = $options{'gen-parrot'};
+        my ($par_want) = split(' ', slurp('tools/build/PARROT_REVISION'));
+
+        if (defined $gen_parrot) {
+            $with_parrot = gen_parrot($par_want, %options, prefix => $prefix);
+        }
+
+        my @errors;
+
+        my %par_config;
+        if ($with_parrot) {
+            %par_config = read_parrot_config($with_parrot)
+            or push @errors, "Unable to read configuration from $with_parrot.";
+        }
+        else {
+            %par_config = read_parrot_config("$prefix/bin/parrot$exe", "parrot$exe")
+            or push @errors, "Unable to find parrot.";
+            $with_parrot = fill_template_text('@bindir@/parrot@exe@', %par_config);
+        }
+
+        %config = (%config, %par_config);
+        my $par_have = $config{'parrot::git_describe'} || '';
+        if ($par_have && cmp_rev($par_have, $par_want) < 0) {
+            push @errors, "Parrot revision $par_want required (currently $par_have).";
+        }
+
+        if (!@errors) {
+            push @errors, verify_install([@NQP::Configure::required_parrot_files],
+                                        %config);
+            push @errors,
+            "(Perhaps you need to 'make install', 'make install-dev',",
+            "or install the 'devel' package for Parrot?)"
+            if @errors;
+        }
+
+        if (@errors && !defined $gen_parrot) {
+            push @errors,
+            "\nTo automatically clone (git) and build a copy of Parrot $par_want,",
+            "try re-running Configure.pl with the '--gen-parrot' option.",
+            "Or, use '--with-parrot=' to explicitly specify the Parrot",
+            "executable to use to build NQP.";
+        }
+
+        sorry(@errors) if @errors;
+
+        print "Using $with_parrot (version $config{'parrot::git_describe'}).\n";
+
+        if ($^O eq 'MSWin32' or $^O eq 'cygwin') {
+            $config{'dll'} = '$(PARROT_BIN_DIR)/$(PARROT_LIB_SHARED)';
+            $config{'dllcopy'} = '$(PARROT_LIB_SHARED)';
+            $config{'make_dllcopy'} =
+                '$(PARROT_DLL_COPY) : $(PARROT_DLL)'."\n\t".'$(CP) $(PARROT_DLL) .';
+        }
+
+        my $make = fill_template_text('@make@', %config);
+
+        if ($make eq 'nmake') {
+            system_or_die('cd 3rdparty\dyncall && Configure.bat' .
+                ($config{'parrot::archname'} =~ /x64/ ? ' /target-x64' : ''));
+            $config{'dyncall_build'} = 'cd 3rdparty\dyncall && nmake Nmakefile';
+        }
+        else {
+            if ($^O eq 'MSWin32') {
+                my $configure_args =
+                    $config{'parrot::archname'} =~ /x86/ ? ' /target-x86' : ' /target-x64';
+
+                $configure_args   .= $config{'parrot::cc'} eq 'gcc' ? ' /tool-gcc' : '';
+
+                system_or_die('cd 3rdparty\dyncall && Configure.bat' . $configure_args);
+                $config{'dyncall_build'} = "cd 3rdparty/dyncall && $make BUILD_DIR=. -f Makefile.embedded mingw32";
+            } else {
+                my $target_args = '';
+                # heuristic according to
+                # https://github.com/perl6/nqp/issues/100#issuecomment-18523608
+                if ($^O eq 'darwin' && qx/ld 2>&1/ =~ /inferred architecture x86_64/) {
+                    $target_args = " --target-x64";
+                }
+                system_or_die('cd 3rdparty/dyncall && sh configure' . $target_args);
+
+                if ($^O eq 'netbsd') {
+                    $config{'dyncall_build'} = "cd 3rdparty/dyncall && BUILD_DIR=. $make -f BSDmakefile";
+                } else {
+                    $config{'dyncall_build'} = "cd 3rdparty/dyncall && BUILD_DIR=. $make";
+                }
+            }
+        }
+
+        fill_template_file(
+            ['tools/build/Makefile-common.in', 'tools/build/Makefile-Parrot.in'], 'Makefile',
+            %config,
+        );
+        fill_template_file('src/vm/parrot/nqp.sh', 'gen/parrot/nqp_launcher', %config);
+        chmod 0755, 'gen/parrot/nqp_launcher';
+    }
+    if ($backends{moar}) {
+        my @errors;
+        my $moar_path = "$prefix${slash}bin${slash}moar" . ($^O =~ /MSWin32/ ? '.exe' : '');
+        my @moar_info = `$moar_path --help`;
+        my $moar_found = 0;
+        for (@moar_info) {
+            if (/USAGE: moar/) {
+                $moar_found = 1;
+                last;
+            }
+        }
+        if (!$moar_found) {
+            push @errors,
+                "No MoarVM (moar executable) found using the --prefix";
+        }
+        sorry(@errors) if @errors;
+        $config{'make'}   = $^O eq 'MSWin32' ? 'nmake' : 'make';
+        $config{'runner'} = $^O eq 'MSWin32' ? 'nqp.bat' : 'nqp';
+        fill_template_file(
+            ['tools/build/Makefile-common.in', 'tools/build/Makefile-Moar.in'],
+            'Makefile',
+            %config,
+        );
+
     }
 
     my $make = fill_template_text('@make@', %config);
-
-    if ($make eq 'nmake') {
-        system_or_die('cd 3rdparty\dyncall && Configure.bat' .
-            ($config{'parrot::archname'} =~ /x64/ ? ' /target-x64' : ''));
-        $config{'dyncall_build'} = 'cd 3rdparty\dyncall && nmake Nmakefile';
-    }
-    else {
-        if ($^O eq 'MSWin32') {
-            my $configure_args =
-                $config{'parrot::archname'} =~ /x86/ ? ' /target-x86' : ' /target-x64';
-
-            $configure_args   .= $config{'parrot::cc'} eq 'gcc' ? ' /tool-gcc' : '';
-
-            system_or_die('cd 3rdparty\dyncall && Configure.bat' . $configure_args);
-            $config{'dyncall_build'} = "cd 3rdparty/dyncall && $make BUILD_DIR=. -f Makefile.embedded mingw32";
-        } else {
-            my $target_args = '';
-            # heuristic according to
-            # https://github.com/perl6/nqp/issues/100#issuecomment-18523608
-            if ($^O eq 'darwin' && qx/ld 2>&1/ =~ /inferred architecture x86_64/) {
-                $target_args = " --target-x64";
-            }
-            system_or_die('cd 3rdparty/dyncall && sh configure' . $target_args);
-
-            if ($^O eq 'netbsd') {
-                $config{'dyncall_build'} = "cd 3rdparty/dyncall && BUILD_DIR=. $make -f BSDmakefile";
-            } else {
-                $config{'dyncall_build'} = "cd 3rdparty/dyncall && BUILD_DIR=. $make";
-            }
-        }
-    }
-
-    fill_template_file(
-        ['tools/build/Makefile-common.in', 'tools/build/Makefile-Parrot.in'], 'Makefile',
-        %config,
-    );
-    fill_template_file('src/vm/parrot/nqp.sh', 'gen/parrot/nqp_launcher', %config);
-    chmod 0755, 'gen/parrot/nqp_launcher';
-
-    {
+    unless ($options{'no-clean'}) {
         no warnings;
         print "Cleaning up ...\n";
         if (open my $CLEAN, '-|', "$make clean") {
@@ -181,6 +234,7 @@ Configure.pl - NQP Configure
 General Options:
     --help             Show this text
     --prefix=dir       Install files in dir
+    --backends=list    Backends to use: parrot,jvm,moar
     --with-parrot=path/to/bin/parrot
                        Parrot executable to use to build NQP
     --gen-parrot[=branch]
