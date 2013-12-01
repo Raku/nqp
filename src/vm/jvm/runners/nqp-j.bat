f40f1159f7b19c801a2052f07b8cb23fd6b6c5b4
Arcterus <Arcterus@mail.com>
2013-12-01 13:55:25 -0800

    Let nqp-j work even if CLASSPATH is set

--- src/vm/jvm/runners/nqp-j.bat
+++ src/vm/jvm/runners/nqp-j.bat
@@ -1,1 +1,1 @@
-@java -Xbootclasspath/a:.;nqp-runtime.jar;3rdparty/asm/asm-4.1.jar;3rdparty/jline/jline-1.0.jar;3rdparty/jna/jna.jar;nqp.jar nqp %*
+@java -cp . -Xbootclasspath/a:.;nqp-runtime.jar;3rdparty/asm/asm-4.1.jar;3rdparty/jline/jline-1.0.jar;3rdparty/jna/jna.jar;nqp.jar nqp %*
