61dbd666f5de712ac58ef996729ffeb7e5e3cb8f
Tobias Leich <email@froggs.de>
2014-07-02 22:11:39 +0200

    allow CPointer math for jvm backend
    The memory address of a CPointer can now be read and set.

--- src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/CPointer.java
+++ src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/CPointer.java
@@ -1,5 +1,8 @@
 package org.perl6.nqp.sixmodel.reprs;
 
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 import org.perl6.nqp.sixmodel.REPR;
 import org.perl6.nqp.sixmodel.SerializationReader;
 import org.perl6.nqp.sixmodel.SixModelObject;
@@ -23,6 +26,23 @@ public class CPointer extends REPR {
         SixModelObject obj = new CPointerInstance();
         obj.st = st;
         return obj;
+    }
+
+    public void generateBoxingMethods(ThreadContext tc, STable st, ClassWriter cw, String className, String prefix) {
+        String getDesc = "(Lorg/perl6/nqp/runtime/ThreadContext;)J";
+        MethodVisitor getMeth = cw.visitMethod(Opcodes.ACC_PUBLIC, "get_int", getDesc, null, null);
+        getMeth.visitVarInsn(Opcodes.ALOAD, 0);
+        getMeth.visitFieldInsn(Opcodes.GETFIELD, className, prefix, "J");
+        getMeth.visitInsn(Opcodes.LRETURN);
+        getMeth.visitMaxs(0, 0);
+
+        String setDesc = "(Lorg/perl6/nqp/runtime/ThreadContext;J)V";
+        MethodVisitor setMeth = cw.visitMethod(Opcodes.ACC_PUBLIC, "set_int", setDesc, null, null);
+        setMeth.visitVarInsn(Opcodes.ALOAD, 0);
+        setMeth.visitVarInsn(Opcodes.LLOAD, 2);
+        setMeth.visitFieldInsn(Opcodes.PUTFIELD, className, prefix, "J");
+        setMeth.visitInsn(Opcodes.RETURN);
+        setMeth.visitMaxs(0, 0);
     }
 
     public SixModelObject deserialize_stub(ThreadContext tc, STable st) {
--- src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/CPointerInstance.java
+++ src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/CPointerInstance.java
@@ -2,8 +2,21 @@ package org.perl6.nqp.sixmodel.reprs;
 
 import com.sun.jna.Pointer;
 
+import org.perl6.nqp.runtime.ThreadContext;
 import org.perl6.nqp.sixmodel.SixModelObject;
 
 public class CPointerInstance extends SixModelObject {
     public Pointer pointer;
+
+    public void set_int(ThreadContext tc, long value) {
+        this.pointer = (value > 0)
+            ? Pointer.createConstant(value)
+            : null;
+    }
+    
+    public long get_int(ThreadContext tc) {
+        return this.pointer == null
+            ? 0 
+            : Pointer.nativeValue(this.pointer);
+    }
 }
