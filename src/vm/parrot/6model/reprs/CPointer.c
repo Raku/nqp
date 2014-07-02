816b28176ad71ce072705cfe83753c4341bf0a3c
Tobias Leich <email@froggs.de>
2014-07-02 21:16:01 +0200

    allow CPointer math for parrot backend
    The memory address of a CPointer can now be read and set.

--- src/vm/parrot/6model/reprs/CPointer.c
+++ src/vm/parrot/6model/reprs/CPointer.c
@@ -56,6 +56,54 @@ static void copy_to(PARROT_INTERP, STable *st, void *src, void *dest) {
     dest_body->ptr = src_body->ptr;
 }
 
+/* Used with boxing. Sets an integer value, for representations that can hold
+ * one. */
+static void set_int(PARROT_INTERP, STable *st, void *data, INTVAL value) {
+    ((CPointerBody *)data)->ptr = (void *)value;
+}
+
+/* Used with boxing. Gets an integer value, for representations that can
+ * hold one. */
+static INTVAL get_int(PARROT_INTERP, STable *st, void *data) {
+    return (Parrot_Int8)((CPointerBody *)data)->ptr;
+}
+
+/* Used with boxing. Sets a floating point value, for representations that can
+ * hold one. */
+static void set_num(PARROT_INTERP, STable *st, void *data, FLOATVAL value) {
+    Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_INVALID_OPERATION,
+            "CPointer cannot box a native num");
+}
+
+/* Used with boxing. Gets a floating point value, for representations that can
+ * hold one. */
+static FLOATVAL get_num(PARROT_INTERP, STable *st, void *data) {
+    Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_INVALID_OPERATION,
+            "CPointer cannot unbox to a native num");
+}
+
+/* Used with boxing. Sets a string value, for representations that can hold
+ * one. */
+static void set_str(PARROT_INTERP, STable *st, void *data, STRING *value) {
+    Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_INVALID_OPERATION,
+            "CPointer cannot box a native string");
+}
+
+/* Used with boxing. Gets a string value, for representations that can hold
+ * one. */
+static STRING * get_str(PARROT_INTERP, STable *st, void *data) {
+    Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_INVALID_OPERATION,
+            "CPointer cannot unbox to a native string");
+}
+
+/* Some objects serve primarily as boxes of others, inlining them. This gets
+ * gets the reference to such things, using the representation ID to distinguish
+ * them. */
+static void * get_boxed_ref(PARROT_INTERP, STable *st, void *data, INTVAL repr_id) {
+    Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_INVALID_OPERATION,
+            "CPointer cannot box other types");
+}
+
 /* This Parrot-specific addition to the API is used to free an object. */
 static void gc_free(PARROT_INTERP, PMC *obj) {
     mem_sys_free(PMC_data(obj));
@@ -88,6 +136,14 @@ REPROps * CPointer_initialize(PARROT_INTERP,
     this_repr->allocate = allocate;
     this_repr->initialize = initialize;
     this_repr->copy_to = copy_to;
+    this_repr->box_funcs = mem_allocate_typed(REPROps_Boxing);
+    this_repr->box_funcs->set_int = set_int;
+    this_repr->box_funcs->get_int = get_int;
+    this_repr->box_funcs->set_num = set_num;
+    this_repr->box_funcs->get_num = get_num;
+    this_repr->box_funcs->set_str = set_str;
+    this_repr->box_funcs->get_str = get_str;
+    this_repr->box_funcs->get_boxed_ref = get_boxed_ref;
     this_repr->gc_free = gc_free;
     this_repr->get_storage_spec = get_storage_spec;
     return this_repr;
