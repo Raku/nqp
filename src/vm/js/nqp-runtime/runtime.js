8e53d12404bf1b67be643ad0ccc6093432fd3a26
Pawel Murias <pawelmurias@gmail.com>
2014-08-14 17:48:29 +0200

    Add missing nqp.named, fix indentation.

--- src/vm/js/nqp-runtime/runtime.js
+++ src/vm/js/nqp-runtime/runtime.js
@@ -19,7 +19,11 @@ exports.to_str = function(arg) {
   }
 };
 
+exports.named = function(named) {
+  return named;
+}
+
 // Placeholder
 exports.top_context = function() {
-    return null;
+  return null;
 }
