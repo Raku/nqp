48e5adf2e2c8d7def4f22cd302e087225affa93c
Pawel Murias <pawelmurias@gmail.com>
2014-08-18 17:11:20 +0200

    Use webpack-reload-plugin for the webpacking example, and mention that in the README.

--- examples/webpacked/webpack.config.js
+++ examples/webpacked/webpack.config.js
@@ -1,6 +1,8 @@
+var ReloadPlugin = require("webpack-reload-plugin");
 module.exports = {
   entry: "./example.nqp",
   nqpRepo: "../..", // Edit this to specify where the nqp-js repo is
+  plugins: [new ReloadPlugin("localhost")],
   output: {
     path: __dirname,
     filename: "bundle.js"
