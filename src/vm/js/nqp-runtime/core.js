ab04e514ce8521a96e24cd0e3be4e4da8c8e1207
Pawel Murias <pawelmurias@gmail.com>
2014-09-19 16:06:57 +0200

    Seperate some ops into a 'core' ops module.

---
+++ src/vm/js/nqp-runtime/core.js
@@ -0,0 +1,132 @@
+var op = {};
+exports.op = op;
+
+op.getcomp = function(lang) {
+  if (lang == 'JavaScript') {
+    return function(ctx, named, code) {
+      return eval(code);
+    };
+  }
+};
+
+op.isinvokable = function(obj) {
+  return (typeof obj == 'function' ? 1 : 0);
+};
+
+op.escape = function(str) {
+  return str
+    .replace(/\\/g, '\\\\')
+    .replace(/\x1B/g, '\\e')
+    .replace(/\n/g, '\\n')
+    .replace(/\r/g, '\\r')
+    .replace(/\t/g, '\\t')
+    .replace(/\f/g, '\\f')
+    .replace(/[\b]/g, '\\b')
+    .replace(/"/g, '\\"');
+};
+
+op.x = function(str, times) {
+  if (!times) return '';
+  var ret = str;
+  while (--times) ret += str;
+  return ret;
+};
+
+op.radix = function(radix, str, zpos, flags) {
+  if (flags != 2) {throw 'flags != 2 not implemented yet: ' + flags}
+
+  var lowercase = 'a-' + String.fromCharCode('a'.charCodeAt(0) + radix - 11);
+  var uppercase = 'A-' + String.fromCharCode('A'.charCodeAt(0) + radix - 11);
+
+  var letters = radix >= 11 ? lowercase + uppercase : '';
+
+  var digitclass = '[0-' + Math.min(radix - 1, 9) + letters + ']';
+  var minus = flags & 0x02 ? '-?' : '';
+  var regex = new RegExp(
+      '^' + minus + digitclass + '(?:_' +
+      digitclass + '|' + digitclass + ')*');
+  var str = str.slice(zpos);
+  var search = str.match(regex);
+  if (search == null) {
+    return [0, 1, -1];
+  }
+  var number = search[0].replace(/_/g, '');
+  var power = number[0] == '-' ? number.length - 1 : number.length;
+  var pow = Math.pow(radix, power);
+  var ret = [parseInt(number, radix), pow, zpos + search[0].length];
+  return ret;
+};
+
+function Iter(array) {
+  this.array = array;
+  this.target = array.length;
+  this.idx = 0;
+}
+
+Iter.prototype.shift = function() {
+  return this.array[this.idx++];
+};
+
+Iter.prototype.$$to_bool = function() {
+   return arg.idx < arg.target;
+};
+
+function HashIter(hash) {
+  this.hash = hash;
+  this.keys = Object.keys(hash);
+  this.target = this.keys.length;
+  this.idx = 0;
+}
+
+HashIter.prototype.shift = function() {
+  return new IterPair(this.hash, this.keys[this.idx++]);
+};
+
+HashIter.prototype.$$to_bool = function() {
+   return arg.idx < arg.target;
+};
+
+function IterPair(hash, key) {
+  this._key = key;
+  this._hash = hash;
+}
+
+IterPair.prototype.iterval = function() {
+  return this._hash[this._key];
+};
+IterPair.prototype.iterkey_s = function() {
+  return this._key;
+};
+
+IterPair.prototype.key = function(ctx, named) {
+  return this._key;
+};
+IterPair.prototype.value = function(ctx, named) {
+  return this._hash[this._key];
+};
+
+
+op.iterator = function(obj) {
+  if (obj instanceof Array) {
+    return new Iter(obj);
+  } else if (obj instanceof Hash) {
+    return new HashIter(obj);
+  } else {
+    throw 'unsupported thing to iterate over';
+  }
+};
+
+function Hash() {
+}
+
+exports.hash = function() {
+  return new Hash();
+};
+exports.op.ishash = function(obj) {
+  return obj instanceof Hash ? 1 : 0;
+};
+
+op.existspos = function(array, idx) {
+  if (idx < 0) idx += array.length;
+  return boolish(array.hasOwnProperty(idx));
+};
--- src/vm/js/nqp-runtime/runtime.js
+++ src/vm/js/nqp-runtime/runtime.js
@@ -1,10 +1,20 @@
 var op = {};
 exports.op = op;
 
+function load_ops(module) {
+  for (var name in module.op) {
+    op[name] = module.op[name];
+  }
+}
+
+var core = require('./core');
+load_ops(core);
+exports.hash = core.hash;
 
 op.print = function(arg) {
   process.stdout.write(arg);
 };
+
 op.say = function(arg) {
   if (process.stdout) {
     process.stdout.write(arg);
@@ -14,112 +24,6 @@ op.say = function(arg) {
   }
 };
 
-op.getcomp = function(lang) {
-  if (lang == 'JavaScript') {
-    return function(ctx, named, code) {
-      return eval(code);
-    };
-  }
-};
-
-op.isinvokable = function(obj) {
-  return (typeof obj == 'function' ? 1 : 0);
-};
-
-op.escape = function(str) {
-  return str
-    .replace(/\\/g, '\\\\')
-    .replace(/\x1B/g, '\\e')
-    .replace(/\n/g, '\\n')
-    .replace(/\r/g, '\\r')
-    .replace(/\t/g, '\\t')
-    .replace(/\f/g, '\\f')
-    .replace(/[\b]/g, '\\b')
-    .replace(/"/g, '\\"');
-};
-
-op.x = function(str, times) {
-  if (!times) return '';
-  var ret = str;
-  while (--times) ret += str;
-  return ret;
-};
-
-op.radix = function(radix, str, zpos, flags) {
-  if (flags != 2) {throw 'flags != 2 not implemented yet: ' + flags}
-
-  var lowercase = 'a-' + String.fromCharCode('a'.charCodeAt(0) + radix - 11);
-  var uppercase = 'A-' + String.fromCharCode('A'.charCodeAt(0) + radix - 11);
-
-  var letters = radix >= 11 ? lowercase + uppercase : '';
-
-  var digitclass = '[0-' + Math.min(radix - 1, 9) + letters + ']';
-  var minus = flags & 0x02 ? '-?' : '';
-  var regex = new RegExp(
-      '^' + minus + digitclass + '(?:_' +
-      digitclass + '|' + digitclass + ')*');
-  var str = str.slice(zpos);
-  var search = str.match(regex);
-  if (search == null) {
-    return [0, 1, -1];
-  }
-  var number = search[0].replace(/_/g, '');
-  var power = number[0] == '-' ? number.length - 1 : number.length;
-  var pow = Math.pow(radix, power);
-  var ret = [parseInt(number, radix), pow, zpos + search[0].length];
-  return ret;
-};
-
-function Iter(array) {
-  this.array = array;
-  this.target = array.length;
-  this.idx = 0;
-}
-
-Iter.prototype.shift = function() {
-  return this.array[this.idx++];
-};
-
-function HashIter(hash) {
-  this.hash = hash;
-  this.keys = Object.keys(hash);
-  this.target = this.keys.length;
-  this.idx = 0;
-}
-
-HashIter.prototype.shift = function() {
-  return new IterPair(this.hash, this.keys[this.idx++]);
-};
-
-function IterPair(hash, key) {
-  this._key = key;
-  this._hash = hash;
-}
-
-IterPair.prototype.iterval = function() {
-  return this._hash[this._key];
-};
-IterPair.prototype.iterkey_s = function() {
-  return this._key;
-};
-
-IterPair.prototype.key = function(ctx, named) {
-  return this._key;
-};
-IterPair.prototype.value = function(ctx, named) {
-  return this._hash[this._key];
-};
-
-
-op.iterator = function(obj) {
-  if (obj instanceof Array) {
-    return new Iter(obj);
-  } else if (obj instanceof Hash) {
-    return new HashIter(obj);
-  } else {
-    throw 'unsupported thing to iterate over';
-  }
-};
 
 exports.to_str = function(arg) {
   if (typeof arg == 'number') {
@@ -161,10 +65,10 @@ exports.to_bool = function(arg) {
     return arg == '' || arg == '0' ? 0 : 1;
   } else if (arg instanceof Array) {
     return arg.length == 0 ? 0 : 1;
-  } else if (arg instanceof Iter || arg instanceof HashIter) {
-    return arg.idx < arg.target;
   } else if (arg === undefined) {
     return 0;
+  } else if (arg.$$to_bool) {
+    return arg.$$to_bool();
   } else {
     throw "Can't decide if arg is true";
   }
@@ -185,28 +89,11 @@ if (!Math.imul) {
   };
 }
 
-
-function Hash() {
-}
-
-exports.hash = function() {
-  return new Hash();
-};
-exports.op.ishash = function(obj) {
-  return obj instanceof Hash ? 1 : 0;
-};
-
-
 // Placeholder
 exports.top_context = function() {
   return null;
 };
 
-
-op.existspos = function(array, idx) {
-  if (idx < 0) idx += array.length;
-  return boolish(array.hasOwnProperty(idx));
-};
 
 // Serialization stubs
 
@@ -278,4 +165,3 @@ op.stat = function(file, code) {
     case PLATFORM_BLOCKS: return stats.blocks;
   }
 };
-
