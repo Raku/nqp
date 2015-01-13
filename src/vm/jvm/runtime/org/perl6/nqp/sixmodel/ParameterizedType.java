497c8ffd3b5532cf8c15efd66d19f84319d88cd2
jnthn <jnthn@jnthn.net>
2015-01-13 22:48:32 +0100

    Basic implementation of parametric ops on JVM.
    Passes 11 out of the 14 tests that exist so far.

--- src/vm/jvm/runtime/org/perl6/nqp/sixmodel/ParameterizedType.java
+++ src/vm/jvm/runtime/org/perl6/nqp/sixmodel/ParameterizedType.java
@@ -2,8 +2,8 @@ package org.perl6.nqp.sixmodel;
 
 public final class ParameterizedType extends AbstractParametricity {
     /* The type that we are a parameterization of. */
-    SixModelObject parametric_type;
+    public SixModelObject parametricType;
 
     /* Our type parameters. */
-    SixModelObject parameters;
+    public SixModelObject parameters;
 }
--- src/vm/jvm/runtime/org/perl6/nqp/sixmodel/ParametricType.java
+++ src/vm/jvm/runtime/org/perl6/nqp/sixmodel/ParametricType.java
@@ -1,12 +1,12 @@
 package org.perl6.nqp.sixmodel;
 
+import java.util.List;
+import java.util.Map;
+
 public final class ParametricType extends AbstractParametricity {
     /* The code object to use to produce a new parameterization. */
-    SixModelObject parameterizer;
+    public SixModelObject parameterizer;
 
-    /* Lookup table of existing parameterizations. For now, just a VM
-     * array with alternating pairs of [arg array], object. Could in
-     * the future we something lower level or hashy; we've yet to see
-     * how hot-path lookups end up being in reality. */
-    SixModelObject lookup;
+    /* Lookup table of existing parameterizations. */
+    public List<Map.Entry<SixModelObject, SixModelObject>> lookup;
 }
