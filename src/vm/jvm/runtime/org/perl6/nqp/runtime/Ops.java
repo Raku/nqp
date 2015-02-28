228f135ab2b80051478615e234078bcd8ab44a00
jnthn <jnthn@jnthn.net>
2015-02-28 14:00:28 +0100

    Fix native ref type HLL config thinko.

--- src/vm/jvm/runtime/org/perl6/nqp/runtime/Ops.java
+++ src/vm/jvm/runtime/org/perl6/nqp/runtime/Ops.java
@@ -4852,23 +4852,23 @@ public final class Ops {
         if (configHash.exists_key(tc, "exit_handler") != 0)
             config.exitHandler = configHash.at_key_boxed(tc, "exit_handler");
         if (configHash.exists_key(tc, "int_lex_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "int_lex_ref");
+            config.intLexRef = configHash.at_key_boxed(tc, "int_lex_ref");
         if (configHash.exists_key(tc, "num_lex_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "num_lex_ref");
+            config.numLexRef = configHash.at_key_boxed(tc, "num_lex_ref");
         if (configHash.exists_key(tc, "str_lex_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "str_lex_ref");
+            config.strLexRef = configHash.at_key_boxed(tc, "str_lex_ref");
         if (configHash.exists_key(tc, "int_attr_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "int_attr_ref");
+            config.intAttrRef = configHash.at_key_boxed(tc, "int_attr_ref");
         if (configHash.exists_key(tc, "num_attr_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "num_attr_ref");
+            config.numAttrRef = configHash.at_key_boxed(tc, "num_attr_ref");
         if (configHash.exists_key(tc, "str_attr_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "str_attr_ref");
+            config.strAttrRef = configHash.at_key_boxed(tc, "str_attr_ref");
         if (configHash.exists_key(tc, "int_pos_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "int_pos_ref");
+            config.intPosRef = configHash.at_key_boxed(tc, "int_pos_ref");
         if (configHash.exists_key(tc, "num_pos_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "num_pos_ref");
+            config.numPosRef = configHash.at_key_boxed(tc, "num_pos_ref");
         if (configHash.exists_key(tc, "str_pos_ref") != 0)
-            config.exitHandler = configHash.at_key_boxed(tc, "str_pos_ref");
+            config.strPosRef = configHash.at_key_boxed(tc, "str_pos_ref");
         return configHash;
     }
     public static SixModelObject getcomp(String name, ThreadContext tc) {
