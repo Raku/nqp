404e84224ed2e2acce9d1d67ae424a09fd89359e
Jonathan Worthington <jnthn@jnthn.net>
2015-07-10 17:15:28 +0200

    Implement multi-dim access funcs in VMArray.

--- src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/VMArrayInstanceBase.java
+++ src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/VMArrayInstanceBase.java
@@ -16,4 +16,28 @@ public class VMArrayInstanceBase extends SixModelObject {
             throw ExceptionHandling.dieInternal(tc, "A dynamic array can only have a single dimension");
         this.set_elems(tc, dims[0]);
     }
+
+    public SixModelObject at_pos_multidim_boxed(ThreadContext tc, long[] indices) {
+        if (indices.length != 1)
+            throw ExceptionHandling.dieInternal(tc, "A dynamic array can only be indexed with a single dimension");
+        return this.at_pos_boxed(tc, indices[0]);
+    }
+
+    public void at_pos_multidim_native(ThreadContext tc, long[] indices) {
+        if (indices.length != 1)
+            throw ExceptionHandling.dieInternal(tc, "A dynamic array can only be indexed with a single dimension");
+        this.at_pos_native(tc, indices[0]);
+    }
+
+    public void bind_pos_multidim_boxed(ThreadContext tc, long[] indices, SixModelObject value) {
+        if (indices.length != 1)
+            throw ExceptionHandling.dieInternal(tc, "A dynamic array can only be indexed with a single dimension");
+        this.bind_pos_boxed(tc, indices[0], value);
+    }
+
+    public void bind_pos_multidim_native(ThreadContext tc, long[] indices) {
+        if (indices.length != 1)
+            throw ExceptionHandling.dieInternal(tc, "A dynamic array can only be indexed with a single dimension");
+        this.bind_pos_native(tc, indices[0]);
+    }
 }
