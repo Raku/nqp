f7c477754babd263b03f8791552e95c3bcb1f826
Pawel Murias <pawelmurias@gmail.com>
2015-08-15 12:27:32 +0200

    Add whitespace

--- src/vm/js/nqp-runtime/bignum.js
+++ src/vm/js/nqp-runtime/bignum.js
@@ -49,43 +49,56 @@ op.tostr_I = function(n) {
 op.base_I = function(n, base) {
   return getBI(n).toString(base).toUpperCase();
 };
+
 op.iseq_I = function(a, b) {
   return intish_bool(getBI(a).eq(getBI(b)));
 };
+
 op.mul_I = function(a, b, type) {
   return makeBI(type, getBI(a).mul(getBI(b)));
 };
+
 op.add_I = function(a, b, type) {
   return makeBI(type, getBI(a).add(getBI(b)));
 };
+
 op.sub_I = function(a, b, type) {
   return makeBI(type, getBI(a).sub(getBI(b)));
 };
+
 op.sub_I = function(a, b, type) {
   return makeBI(type, getBI(a).sub(getBI(b)));
 };
+
 op.div_I = function(a, b, type) {
   return makeBI(type, getBI(a).div(getBI(b)));
 };
+
 op.pow_I = function(a, b, type) {
   return makeBI(type, getBI(a).pow(getBI(b)));
 };
+
 op.expmod_I = function(a, b, c, type) {
   return makeBI(type, getBI(a).powm(getBI(b), getBI(c)));
 };
+
 op.div_In = function(a, b) {
   var digits = 1e+20;
   return getBI(a).mul(bigint(digits)).div(getBI(b)).toNumber()/digits;
 };
+
 op.rand_I = function(max, type) {
   return makeBI(type, getBI(max).rand());
 };
+
 op.isle_I = function(a, b) {
   return intish_bool(getBI(a).le(getBI(b)));
 };
+
 op.islt_I = function(a, b) {
   return intish_bool(getBI(a).le(getBI(b)));
 };
+
 op.isprime_I = function(n) {
   return intish_bool(getBI(n).probPrime(50));
 };
@@ -93,29 +106,37 @@ op.isprime_I = function(n) {
 op.bitshiftl_I = function(a, b, type) {
   return makeBI(type, getBI(a).shiftLeft(b));
 };
+
 op.bitshiftr_I = function(a, b, type) {
   return makeBI(type, getBI(a).shiftRight(b));
 };
+
 op.bitshiftr_I = function(a, b, type) {
   return makeBI(type, getBI(a).shiftRight(b));
 };
+
 op.bitand_I = function(a, b, type) {
   return makeBI(type, getBI(a).and(getBI(b)));
 };
+
 op.bitor_I = function(a, b, type) {
   return makeBI(type, getBI(a).or(getBI(b)));
 };
+
 op.bitxor_I = function(a, b, type) {
   return makeBI(type, getBI(a).xor(getBI(b)));
 };
+
 op.bitneg_I = function(a, type) {
   return makeBI(type, getBI(a).neg().sub(1));
 };
+
 op.lcm_I = function(n, m, type) {
   var a = getBI(n);
   var b = getBI(m);
   return makeBI(type, (a.abs().div(a.gcd(b)).mul(b.abs())));
 };
+
 op.gcd_I = function(a, b, type) {
   return makeBI(type, getBI(a).gcd(getBI(b)));
 };
