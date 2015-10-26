1c943c55c18b1358cd9313f310eb3cdde1dc0614
Pawel Murias <pawelmurias@gmail.com>
2015-10-26 23:43:33 +0100

    [js] Remove the leftovers from the pre-rewrite nqp-js, at this point I'm not using that for inspiration anymore.

--- src/vm/js/old-nqp-runtime-node/runtime.js
+++
@@ -1,8 +0,0 @@
-var core = require('nqp-runtime-core');
-core.require_ops('nqp-runtime-node/ops/bigint.js');
-core.require_ops('nqp-runtime-node/ops/coro.js');
-core.require_ops('nqp-runtime-node/ops/fs.js');
-core.require_ops('nqp-runtime-node/ops/codepointfromname.js');
-var reprs = require('nqp-runtime-core/reprs.js');
-reprs.P6bigint = require('./reprs/P6bigint.js');
-module.exports = core;
