9f87bb9455d374a175d77c33e880ae9d94c92dbc
Jens Rehsack <sno@netbsd.org>
2015-11-04 18:41:15 +0100

    nqp-j: allow experienced user start with other libdir or jars
    To allow dedicated use of another NQP_DIR or another NQP_JAR list for e.g.
    cross-compiling NQP.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- tools/build/install-jvm-runner.pl
+++ tools/build/install-jvm-runner.pl
@@ -13,11 +13,11 @@ unless (File::Spec->file_name_is_absolute($prefix)) {
     $prefix = File::Spec->rel2abs($prefix);
 }
 
-my $jar_dir = File::Spec->catfile($prefix, 'share', 'nqp', 'runtime');
-my $lib_dir = File::Spec->catfile($prefix, 'share', 'nqp', 'lib');
-
 if ($^O eq 'MSWin32') {
+    my $jar_dir = File::Spec->catfile($prefix, 'share', 'nqp', 'runtime');
+    my $lib_dir = File::Spec->catfile($prefix, 'share', 'nqp', 'lib');
     my $install_to = File::Spec->catfile($realpath, 'bin', 'nqp-j.bat');
+
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
     print $fh '@java -Xss1m -Xmx512m -Xbootclasspath/a:' . $jar_dir . '\\nqp-runtime.jar;' .
@@ -26,12 +26,18 @@ if ($^O eq 'MSWin32') {
         or die "Could not close $install_to: $!";
 }
 else {
+    my $nqp_dir = File::Spec->catfile($prefix, qw/share nqp/);
+    my $jar_dir = File::Spec->catfile('${NQP_DIR}', 'runtime');
+    my $lib_dir = File::Spec->catfile('${NQP_DIR}', 'lib');
+    my $jars = "$jar_dir/nqp-runtime.jar:$jar_dir/asm-4.1.jar:$jar_dir/jline-1.0.jar:$lib_dir/nqp.jar";
     my $install_to = File::Spec->catfile($realpath, 'bin', 'nqp-j');
+
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
     print $fh "#!/bin/sh\n";
-    print $fh "exec java -Xss1m -Xmx512m -Xbootclasspath/a:$jar_dir/nqp-runtime.jar:" .
-              "$jar_dir/asm-4.1.jar:$jar_dir/jline-1.0.jar:$lib_dir/nqp.jar -cp $lib_dir nqp \"\$\@\"\n";
+    print $fh ": \${NQP_DIR:=\"$nqp_dir\"}\n";
+    print $fh ": \${NQP_JARS:=\"$jars\"}\n";
+    print $fh "exec java -Xss1m -Xmx512m -Xbootclasspath/a:\${NQP_JARS} -cp $lib_dir nqp \"\$\@\"\n";
     close $fh
         or die "Could not close $install_to: $!";
     chmod 0755, $install_to;
