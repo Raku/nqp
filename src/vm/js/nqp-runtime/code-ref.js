c55ca07b5c87a281778d02a0439e6235051c9d60
Pawel Murias <pawelmurias@gmail.com>
2016-08-15 12:42:00 +0200

    [js] Switch to using EcmaScript6 class syntax for Ctx and CodeRef.

--- src/vm/js/nqp-runtime/code-ref.js
+++ src/vm/js/nqp-runtime/code-ref.js
@@ -1,161 +1,166 @@
 'use strict';
-CodeRef.cuids = {};
 
-function WrappedCtx(wrap) {
-  this.outer = wrap;
-}
-WrappedCtx.prototype.codeRef = function() {
-  return {statiCode: null};
-};
-
-function CodeRef(name, cuid) {
-  this.name = name;
-  this.cuid = cuid;
-  this.staticCode = this;
-  if (cuid) CodeRef.cuids[cuid] = this;
-}
-
-CodeRef.prototype.$$injectMethod = function(proto, name) {
-  if (this.isCompilerStub) {
-    return;
+class WrappedCtx {
+  constructor(wrap) {
+    this.outer = wrap;
   }
-  var codeRefAttr = this.staticCode.codeRefAttr;
-  if (codeRefAttr === null) {
-    return;
-  } else if (codeRefAttr === undefined) {
-    console.warn('unknown codeRefAttr');
-  }
-
-  if (proto.hasOwnProperty(codeRefAttr) && proto[codeRefAttr] !== this) {
-    return;
-  }
-
-  proto[codeRefAttr] = this;
-
-  if (!this.inject) this.inject = [];
-  this.inject.push({name: name, proto: proto});
-
-  if (this.hasOwnProperty('$$call')) {
-    proto[name] = this.$$call;
+  codeRef() {
+    return {statiCode: null};
   }
 };
 
-CodeRef.prototype.capture = function(block) {
-  this.$$call = block;
-  if (this.inject) {
-    for (var i = 0; i < this.inject.length; i++) {
-      this.inject[i].proto[this.inject[i].name] = block;
+class CodeRef {
+  constructor(name, cuid) {
+    this.name = name;
+    this.cuid = cuid;
+    this.staticCode = this;
+    if (cuid) CodeRef.cuids[cuid] = this;
+  }
+
+  $$injectMethod(proto, name) {
+    if (this.isCompilerStub) {
+      return;
+    }
+    var codeRefAttr = this.staticCode.codeRefAttr;
+    if (codeRefAttr === null) {
+      return;
+    } else if (codeRefAttr === undefined) {
+      console.warn('unknown codeRefAttr');
+    }
+
+    if (proto.hasOwnProperty(codeRefAttr) && proto[codeRefAttr] !== this) {
+      return;
+    }
+
+    proto[codeRefAttr] = this;
+
+    if (!this.inject) this.inject = [];
+    this.inject.push({name: name, proto: proto});
+
+    if (this.hasOwnProperty('$$call')) {
+      proto[name] = this.$$call;
     }
   }
-  return this;
-};
 
-CodeRef.prototype.setOuter = function(outerCtx) {
-  this.outerCtx = outerCtx;
-  return this;
-};
-
-// HACK - do this properly
-CodeRef.prototype.$$call = function() {
-  var nqp = require('nqp-runtime');
-  if (this.closureTemplate) {
-
-    var $$cuids = this.cuids;
-    var declare = [];
-    for (var i in $$cuids) {
-      declare.push('cuid' + $$cuids[i].cuid + ' = $$cuids[' + i + ']');
-    }
-    var declareCuids = 'var ' + declare.join(',') + ';\n';
-
-    var searched = this;
-    var forcedOuterCtx = null;
-    while (searched) {
-      if (searched.forcedOuterCtx) {
-        forcedOuterCtx = searched.forcedOuterCtx;
-        break;
+  capture(block) {
+    this.$$call = block;
+    if (this.inject) {
+      for (var i = 0; i < this.inject.length; i++) {
+        this.inject[i].proto[this.inject[i].name] = block;
       }
-      searched = searched.outerCodeRef;
     }
-    var outerCtxVar = this.outerCodeRef.ctx;
+    return this;
+  }
 
-    if (forcedOuterCtx) forcedOuterCtx = new WrappedCtx(forcedOuterCtx);
+  setOuter(outerCtx) {
+    this.outerCtx = outerCtx;
+    return this;
+  }
 
-    var template = declareCuids + 'var ' + outerCtxVar + '= forcedOuterCtx;(' + this.closureTemplate + ')';
-    this.$$call = eval(template);
-    return this.$$call.apply(this, arguments);
+  // HACK - do this properly
+  $$call() {
+    var nqp = require('nqp-runtime');
+    if (this.closureTemplate) {
 
+      var $$cuids = this.cuids;
+      var declare = [];
+      for (var i in $$cuids) {
+        declare.push('cuid' + $$cuids[i].cuid + ' = $$cuids[' + i + ']');
+      }
+      var declareCuids = 'var ' + declare.join(',') + ';\n';
+
+      var searched = this;
+      var forcedOuterCtx = null;
+      while (searched) {
+        if (searched.forcedOuterCtx) {
+          forcedOuterCtx = searched.forcedOuterCtx;
+          break;
+        }
+        searched = searched.outerCodeRef;
+      }
+      var outerCtxVar = this.outerCodeRef.ctx;
+
+      if (forcedOuterCtx) forcedOuterCtx = new WrappedCtx(forcedOuterCtx);
+
+      var template = declareCuids + 'var ' + outerCtxVar + '= forcedOuterCtx;(' + this.closureTemplate + ')';
+      this.$$call = eval(template);
+      return this.$$call.apply(this, arguments);
+
+    }
+  }
+
+  $$apply(argsArray) {
+    return this.$$call.apply(this, argsArray);
+  }
+
+  takeclosure() {
+    console.trace("takeclosure shouldn't be used");
+  }
+
+
+  closure(block) {
+    var closure = new CodeRef(this.name, undefined);
+    closure.codeObj = this.codeObj;
+    closure.cuid = this.cuid;
+    closure.$$call = block;
+    closure.staticCode = this;
+    return closure;
+  }
+
+  CPS(block) {
+    this.$$callCPS = block;
+    return this;
+  }
+
+  sameCPS(block) {
+    this.$$callCPS = function() {
+      var args = Array.prototype.slice.call(arguments);
+      var cont = args.splice(2, 1)[0];
+      return cont(this.$$call.apply(this, args));
+    };
+    return this;
+  }
+
+  onlyCPS(block) {
+    this.$$call = function() {
+      throw 'this block can be only called in CPS mode';
+    };
+    return this;
+  }
+
+  setCodeObj(codeObj) {
+    this.codeObj = codeObj;
+    return this;
+  }
+
+  setInfo(ctx, outerCodeRef, closureTemplate, lexicalsTypeInfo, cuids, setSetting, codeRefAttr) {
+    this.closureTemplate = closureTemplate;
+    this.ctx = ctx;
+    this.outerCodeRef = outerCodeRef;
+    this.lexicalsTypeInfo = lexicalsTypeInfo;
+    this.cuids = cuids;
+    return this;
+  }
+
+  setCodeRefAttr(codeRefAttr) {
+    this.codeRefAttr = codeRefAttr;
+    return this;
+  }
+
+  $$clone() {
+    var clone = new CodeRef(this.name, undefined);
+    clone.$$call = this.$$call;
+    clone.codeObj = this.codeObj;
+    clone.staticCode = this.staticCode;
+    clone.outerCtx = this.outerCtx;
+    return clone;
+  }
+
+  $$toBool(ctx) {
+    return 1;
   }
 };
 
-CodeRef.prototype.$$apply = function(argsArray) {
-  return this.$$call.apply(this, argsArray);
-};
-
-CodeRef.prototype.takeclosure = function() {
-  console.trace("takeclosure shouldn't be used");
-};
-
-
-CodeRef.prototype.closure = function(block) {
-  var closure = new CodeRef(this.name, undefined);
-  closure.codeObj = this.codeObj;
-  closure.cuid = this.cuid;
-  closure.$$call = block;
-  closure.staticCode = this;
-  return closure;
-};
-
-CodeRef.prototype.CPS = function(block) {
-  this.$$callCPS = block;
-  return this;
-};
-
-CodeRef.prototype.sameCPS = function(block) {
-  this.$$callCPS = function() {
-    var args = Array.prototype.slice.call(arguments);
-    var cont = args.splice(2, 1)[0];
-    return cont(this.$$call.apply(this, args));
-  };
-  return this;
-};
-
-CodeRef.prototype.onlyCPS = function(block) {
-  this.$$call = function() {
-    throw 'this block can be only called in CPS mode';
-  };
-  return this;
-};
-
-CodeRef.prototype.setCodeObj = function(codeObj) {
-  this.codeObj = codeObj;
-  return this;
-};
-
-CodeRef.prototype.setInfo = function(ctx, outerCodeRef, closureTemplate, lexicalsTypeInfo, cuids, setSetting, codeRefAttr) {
-  this.closureTemplate = closureTemplate;
-  this.ctx = ctx;
-  this.outerCodeRef = outerCodeRef;
-  this.lexicalsTypeInfo = lexicalsTypeInfo;
-  this.cuids = cuids;
-  return this;
-};
-
-CodeRef.prototype.setCodeRefAttr = function(codeRefAttr) {
-  this.codeRefAttr = codeRefAttr;
-  return this;
-};
-
-CodeRef.prototype.$$clone = function() {
-  var clone = new CodeRef(this.name, undefined);
-  clone.$$call = this.$$call;
-  clone.codeObj = this.codeObj;
-  clone.staticCode = this.staticCode;
-  clone.outerCtx = this.outerCtx;
-  return clone;
-};
-
-CodeRef.prototype.$$toBool = function(ctx) {
-  return 1;
-};
+CodeRef.cuids = {};
 
 module.exports = CodeRef;
--- src/vm/js/nqp-runtime/ctx.js
+++ src/vm/js/nqp-runtime/ctx.js
@@ -1,140 +1,143 @@
+'use strict';
 var CodeRef = require('./code-ref.js');
 var NQPException = require('./nqp-exception.js');
 
-function Ctx(callerCtx, outerCtx, callThis, codeRefAttr) {
-  this.caller = callerCtx;
-  this.outer = outerCtx;
-  this.callThis = callThis;
-  this.codeRefAttr = codeRefAttr;
-}
+class Ctx {
+  constructor(callerCtx, outerCtx, callThis, codeRefAttr) {
+    this.caller = callerCtx;
+    this.outer = outerCtx;
+    this.callThis = callThis;
+    this.codeRefAttr = codeRefAttr;
+  }
 
-Ctx.prototype.codeRef = function() {
-  return (this.callThis instanceof CodeRef ? this.callThis : this.callThis[this.codeRefAttr]);
-};
+  codeRef() {
+    return (this.callThis instanceof CodeRef ? this.callThis : this.callThis[this.codeRefAttr]);
+  }
 
-Ctx.prototype.propagateException = function(exception) {
-  var ctx = this;
-  while (ctx) {
-    if (ctx.CATCH) {
-      exception.caught = ctx;
-      exception.resume = false;
-      ctx.exception = exception;
-      ctx.unwind.ret = ctx.CATCH();
-      if (exception.resume) {
-        return;
-      } else {
-        throw ctx.unwind;
+  propagateException(exception) {
+    var ctx = this;
+    while (ctx) {
+      if (ctx.CATCH) {
+        exception.caught = ctx;
+        exception.resume = false;
+        ctx.exception = exception;
+        ctx.unwind.ret = ctx.CATCH();
+        if (exception.resume) {
+          return;
+        } else {
+          throw ctx.unwind;
+        }
       }
+      ctx = ctx.caller;
     }
-    ctx = ctx.caller;
+    throw exception.message;
   }
-  throw exception.message;
-};
 
-Ctx.prototype.catchException = function(exception) {
-  this.exception = exception;
-  this.CATCH();
-};
-
-Ctx.prototype.rethrow = function(exception) {
-  this.propagateException(exception);
-};
-
-Ctx.prototype.die = function(msg) {
-  this.propagateException(new NQPException(msg));
-};
-
-Ctx.prototype.resume = function(exception) {
-  exception.resume = true;
-};
-
-Ctx.prototype.throw = function(exception) {
-  this.propagateException(exception);
-};
-
-Ctx.prototype.lookupDynamic = function(name) {
-  var ctx = this;
-  while (ctx) {
-    if (ctx.hasOwnProperty(name)) {
-      return ctx[name];
-    }
-    ctx = ctx.caller;
+  catchException(exception) {
+    this.exception = exception;
+    this.CATCH();
   }
-  return null;
-  /* Looking up of a contextual is allowed to fail,
-     nqp code usually fallbacks to looking up of global */
-};
 
-Ctx.prototype.lookupDynamicFromCaller = function(name) {
-  var ctx = this.caller;
-  while (ctx) {
-    if (ctx.hasOwnProperty(name)) {
-      return ctx[name];
-    }
-    ctx = ctx.caller;
+  rethrow(exception) {
+    this.propagateException(exception);
   }
-  return null;
-  /* Looking up of a contextual is allowed to fail,
-     nqp code usually fallbacks to looking up of global */
-};
 
-Ctx.prototype.lookupFromSomeCaller = function(name) {
-  var currentCallerCtx = this.caller;
-  while (currentCallerCtx) {
-    var currentCtx = currentCallerCtx;
-    while (currentCtx) {
-      if (currentCtx.hasOwnProperty(name)) {
-        return currentCtx[name];
+  die(msg) {
+    this.propagateException(new NQPException(msg));
+  }
+
+  resume(exception) {
+    exception.resume = true;
+  }
+
+  throw (exception) {
+    this.propagateException(exception);
+  }
+
+  lookupDynamic(name) {
+    var ctx = this;
+    while (ctx) {
+      if (ctx.hasOwnProperty(name)) {
+        return ctx[name];
       }
-      currentCtx = currentCtx.outer;
+      ctx = ctx.caller;
     }
-    currentCallerCtx = currentCallerCtx.caller;
+    return null;
+    /* Looking up of a contextual is allowed to fail,
+       nqp code usually fallbacks to looking up of global */
   }
-  return null;
-};
 
-Ctx.prototype.lookup = function(name) {
-  var ctx = this;
-  while (ctx) {
-    if (ctx.hasOwnProperty(name)) {
-      return ctx[name];
+  lookupDynamicFromCaller(name) {
+    var ctx = this.caller;
+    while (ctx) {
+      if (ctx.hasOwnProperty(name)) {
+        return ctx[name];
+      }
+      ctx = ctx.caller;
     }
-    ctx = ctx.outer;
+    return null;
+    /* Looking up of a contextual is allowed to fail,
+       nqp code usually fallbacks to looking up of global */
   }
-  /* Rakudo depends on returning null when we can't lookup a lexical */
-  return null;
-};
 
-Ctx.prototype.$$atkey = function(key) {
-  return this.lookup(key);
-};
-
-Ctx.prototype.$$bindkey = function(key, value) {
-  this[key] = value;
-};
-
-Ctx.prototype.bind = function(name, value) {
-  var ctx = this;
-  while (ctx) {
-    if (ctx.hasOwnProperty(name)) {
-      ctx[name] = value;
-      return ctx[name];
+  lookupFromSomeCaller(name) {
+    var currentCallerCtx = this.caller;
+    while (currentCallerCtx) {
+      var currentCtx = currentCallerCtx;
+      while (currentCtx) {
+        if (currentCtx.hasOwnProperty(name)) {
+          return currentCtx[name];
+        }
+        currentCtx = currentCtx.outer;
+      }
+      currentCallerCtx = currentCallerCtx.caller;
     }
-    ctx = ctx.outer;
+    return null;
   }
-  throw "Can't bind: " + name;
-};
 
-Ctx.prototype.bindDynamic = function(name, value) {
-  var ctx = this;
-  while (ctx) {
-    if (ctx.hasOwnProperty(name)) {
-      ctx[name] = value;
-      return ctx[name];
+  lookup(name) {
+    var ctx = this;
+    while (ctx) {
+      if (ctx.hasOwnProperty(name)) {
+        return ctx[name];
+      }
+      ctx = ctx.outer;
     }
-    ctx = ctx.caller;
+    /* Rakudo depends on returning null when we can't lookup a lexical */
+    return null;
   }
-  throw "Can't bind dynamic: " + name;
+
+  $$atkey(key) {
+    return this.lookup(key);
+  }
+
+  $$bindkey(key, value) {
+    this[key] = value;
+  }
+
+  bind(name, value) {
+    var ctx = this;
+    while (ctx) {
+      if (ctx.hasOwnProperty(name)) {
+        ctx[name] = value;
+        return ctx[name];
+      }
+      ctx = ctx.outer;
+    }
+    throw "Can't bind: " + name;
+  }
+
+  bindDynamic(name, value) {
+    var ctx = this;
+    while (ctx) {
+      if (ctx.hasOwnProperty(name)) {
+        ctx[name] = value;
+        return ctx[name];
+      }
+      ctx = ctx.caller;
+    }
+    throw "Can't bind dynamic: " + name;
+  }
 };
 
 module.exports = Ctx;
