2d79a8e5647a33e79f870add0c49d951dd1e9183
Pawel Murias <pawelmurias@gmail.com>
2016-09-22 15:04:55 +0200

    [js] Make nqp::iscclass more correct for most CCLASSes.

--- src/vm/js/nqp-runtime/cclass.js
+++ src/vm/js/nqp-runtime/cclass.js
@@ -1,3 +1,4 @@
+var xregexp = require('xregexp');
 var op = {};
 exports.op = op;
 
@@ -5,35 +6,48 @@ function boolish(bool) {
   return bool ? 1 : 0;
 }
 
+const UPPERCASE = xregexp('^\\p{Lu}');
+const LOWERCASE = xregexp('^\\p{Ll}');
+const ALPHABETIC = xregexp('^\\pL');
+const NUMERIC = xregexp('^\\p{Nd}');
+const HEXADECIMAL = xregexp('^[0-9A-Fa-f]');
+const WHITESPACE = xregexp('^\\p{White_Space}');
+const BLANK = xregexp('^[\t\\p{Zs}]');
+const PUNCTUATION = xregexp('^\\pP');
+const ALPHANUMERIC = xregexp('^[\\pL\\p{Nd}]');
+const WORD = xregexp('^[\\pL_\\p{Nd}]');
+
 function iscclass(cclass, target, offset) {
   if (offset < 0 || offset >= target.length) return 0;
   switch (cclass) {
     //ANY
     case 65535: return 1;
     //UPPERCASE
-    case 1: return boolish(!/^\d|_/.test(target[offset]) && /\w/.test(target[offset]) && target[offset] == target[offset].toUpperCase());
+    case 1: return boolish(UPPERCASE.test(target[offset]));
     //LOWERCASE
-    case 2: return boolish(!/^\d|_/.test(target[offset]) && /\w/.test(target[offset]) && target[offset] == target[offset].toLowerCase());
+    case 2: return boolish(LOWERCASE.test(target[offset]));
     //ALPHABETIC
-    case 4: return boolish(!/^\d|_/.test(target[offset]) && /\w/.test(target[offset]));
+    case 4: return boolish(ALPHABETIC.test(target[offset]));
     //NUMERIC
-    case 8: return boolish(/^\d/.test(target[offset]));
+    case 8: return boolish(NUMERIC.test(target[offset]));
     //HEXADECIMAL
-    case 16: return boolish(/^[0-9a-fA-F]/.test(target[offset]));
+    case 16: return boolish(HEXADECIMAL.test(target[offset]));
     //WHITESPACE
-    case 32: return boolish('\n\u000b\f\r\u0085\u2028\u2029\t \u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000'.indexOf(target[offset]) != -1);
+    case 32: return boolish(WHITESPACE.test(target[offset]));
     //BLANK
-    case 256: return boolish(target[offset] == ' ' || target[offset] == '\t'); //HACK - not all such chars
+    case 256: return boolish(BLANK.test(target[offset]));
     //CONTROL
+    /* TODO - all character*/
     case 512: return boolish('\n\t\r'.indexOf(target[offset]) != -1); //HACK - not all such chars
     //PUNCTUATION
-    case 1024: return boolish(/[.,;?!]/.test(target[offset])); //HACK
+    case 1024: return boolish(PUNCTUATION.test(target[offset])); //HACK
     //ALPHANUMERIC
-    case 2048: return boolish(/^\w/.test(target[offset]) && target[offset] != '_');
+    case 2048: return boolish(ALPHANUMERIC.test(target[offset]));
     //NEWLINE
+    /* TODO - all characters*/
     case 4096: return boolish(target[offset] == '\n' || target[offset] == '\r' || target[offset] == '\u0085'); //HACK
     //WORD
-    case 8192: return boolish(/^\w/.test(target[offset]));
+    case 8192: return boolish(WORD.test(target[offset]));
     default: throw 'cclass ' + cclass + ' not yet implemented';
   }
 }
