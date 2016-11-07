34828da19da0eb256eaaf86c78062fa31c752d70
Pawel Murias <pawelmurias@gmail.com>
2016-11-07 08:48:19 +0100

    [js] Update our nqp-js-on-js test run to take in account changes where the compiled code is stored.

--- src/vm/js/bin/run_tests_bootstrapped.pl
+++ src/vm/js/bin/run_tests_bootstrapped.pl
@@ -3,7 +3,7 @@ use TAP::Harness;
 my $harness = TAP::Harness->new({
     exec => sub {
         my ($harness, $test_file) = @_;
-        return ['node', 'nqp-bootstrapped.js', $test_file] if $test_file =~ /\.t$/;
+        return ['node', 'nqp-js-on-js/nqp-bootstrapped.js', $test_file] if $test_file =~ /\.t$/;
         return ['node', $test_file] if $test_file =~ /\.js$/;
     }
 });
