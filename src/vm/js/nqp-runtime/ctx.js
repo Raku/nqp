7e96e4e68a61933fbb41a20f2e036870a39e0973
Pawel Murias <pawelmurias@gmail.com>
2017-01-09 16:33:17 +0100

    [js] Make nqp::throwpayloadlexcaller skip compiler stubs correctly.

--- src/vm/js/nqp-runtime/ctx.js
+++ src/vm/js/nqp-runtime/ctx.js
@@ -127,9 +127,8 @@ class Ctx extends NQPObject {
 
   throwpayloadlexcaller(category, payload) {
     let ctx = this.$$caller;
-
-    let isThunkOrCompilerStub = code => {code.isThunk || code.isCompilerStub};
-    while (ctx && isThunkOrCompilerStub(ctx.codeRef().staticCode)) {
+    let isThunkOrCompilerStub = code => {return(code.staticCode.isThunk || code.isCompilerStub)};
+    while (ctx && isThunkOrCompilerStub(ctx.codeRef())) {
       ctx = ctx.$$caller;
     }
 
