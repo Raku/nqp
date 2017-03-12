Raku/nqp

commit 042fe376205bbd43e32a71d9ce1980ffd9870ca9

[JVM] Make nqp::div_i round down, not towards zero
This change makes failing test in t/nqp/059-nqpop.t pass.

src/vm/jvm/runtime/org/perl6/nqp/runtime/Ops.java
@@ -6557,6 +6557,10 @@ public final class Ops {
             : new BigDecimal(getBI(tc, a)).divide(new BigDecimal(divisor), 309, RoundingMode.HALF_UP).doubleValue();
     }
 
+    public static long div_i(long a, long b) {
+        return (long)Math.floor((double) a / b);
+    }
+
     public static SixModelObject mod_I(SixModelObject a, SixModelObject b, SixModelObject type, ThreadContext tc) {
         BigInteger divisor = getBI(tc, b, type);
         if (divisor.compareTo(BigInteger.ZERO) < 0) {

src/vm/jvm/QAST/Compiler.nqp
@@ -2223,7 +2223,7 @@ QAST::OperationsJAST.map_jvm_core_op('sub_n', 'dsub', [$RT_NUM, $RT_NUM], $RT_NU
 QAST::OperationsJAST.map_jvm_core_op('mul_i', 'lmul', [$RT_INT, $RT_INT], $RT_INT);
 QAST::OperationsJAST.map_classlib_core_op('mul_I', $TYPE_OPS, 'mul_I', [$RT_OBJ, $RT_OBJ, $RT_OBJ], $RT_OBJ, :tc);
 QAST::OperationsJAST.map_jvm_core_op('mul_n', 'dmul', [$RT_NUM, $RT_NUM], $RT_NUM);
-QAST::OperationsJAST.map_jvm_core_op('div_i', 'ldiv', [$RT_INT, $RT_INT], $RT_INT);
+QAST::OperationsJAST.map_classlib_core_op('div_i', $TYPE_OPS, 'div_i', [$RT_INT, $RT_INT], $RT_INT);
 QAST::OperationsJAST.map_classlib_core_op('div_I', $TYPE_OPS, 'div_I', [$RT_OBJ, $RT_OBJ, $RT_OBJ], $RT_OBJ, :tc);
 QAST::OperationsJAST.map_classlib_core_op('div_In', $TYPE_OPS, 'div_In', [$RT_OBJ, $RT_OBJ], $RT_NUM, :tc);
 QAST::OperationsJAST.map_jvm_core_op('div_n', 'ddiv', [$RT_NUM, $RT_NUM], $RT_NUM);

commit e30967c2f1f048c2e11d43efa984cf2121055d93

[JVM] Handle division by zero in div_i
Commit 042fe37620 introduced a regression (two failing
tests in S32-num/int.t)

src/vm/jvm/runtime/org/perl6/nqp/runtime/Ops.java
@@ -6558,7 +6558,7 @@ public final class Ops {
     }
 
     public static long div_i(long a, long b) {
-        return (long)Math.floor((double) a / b);
+        return (b == 0) ? a / b : (long)Math.floor((double) a / b);
     }
 
     public static SixModelObject mod_I(SixModelObject a, SixModelObject b, SixModelObject type, ThreadContext tc) {