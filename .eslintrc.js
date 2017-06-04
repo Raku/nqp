0ea634c2c306bca01ac49a5f6537a5eae09b9ab7
pmurias <pawelmurias@gmail.com>
2017-06-04 16:24:54 +0200

    [js] Add config file for eslintrc
    There is still a lot of cleanup to the ES6 Google Coding Standard

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+      "ecmaVersion": 6
+    },
+    "rules": {
+      "require-jsdoc": "off",
+      "max-len": "off",
+      "no-var": "off",
+      "arrow-parens": "off",
+      "no-throw-literal": "off",
+      "no-extend-native": "off"
+    }
+};
