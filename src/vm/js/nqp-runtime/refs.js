fb1472bc2af5cc90e6af64c3eed6d2299df2072e
pmurias <pawelmurias@gmail.com>
2017-06-04 16:24:19 +0200

    [js] Use arrow syntax

--- src/vm/js/nqp-runtime/refs.js
+++ src/vm/js/nqp-runtime/refs.js
@@ -41,20 +41,20 @@ function attrRef_s(hllName, get, set) {
 
 op.getattrref_i = function(hllName, obj, classHandle, attrName) {
   return attrRef_i(hllName,
-      function() {return obj.$$getattr_i(classHandle, attrName)},
-      function(value) {obj.$$bindattr_i(classHandle, attrName, value)});
+      () => obj.$$getattr_i(classHandle, attrName),
+      value => obj.$$bindattr_i(classHandle, attrName, value));
 };
 
 op.getattrref_n = function(hllName, obj, classHandle, attrName) {
   return attrRef_n(hllName,
-      function() {return obj.$$getattr_n(classHandle, attrName)},
-      function(value) {obj.$$bindattr_n(classHandle, attrName, value)});
+      () => obj.$$getattr_n(classHandle, attrName),
+      value => obj.$$bindattr_n(classHandle, attrName, value));
 };
 
 op.getattrref_s = function(hllName, obj, classHandle, attrName) {
   return attrRef_s(hllName,
-      function() {return obj.$$getattr_s(classHandle, attrName)},
-      function(value) {obj.$$bindattr_s(classHandle, attrName, value)});
+      () => obj.$$getattr_s(classHandle, attrName),
+      value => obj.$$bindattr_s(classHandle, attrName, value));
 };
 
 helpers.lexRef_i = function(hllName, get, set) {
@@ -100,8 +100,8 @@ op.atposref_i = function(hllName, obj, index) {
   }
   var STable = refType._STable;
   var ref = STable.REPR.allocate(STable);
-  ref.get = function() {return obj.$$atpos(index)};
-  ref.set = function(value) {return obj.$$bindpos(index, value)};
+  ref.get = () => obj.$$atpos(index);
+  ref.set = value => obj.$$bindpos(index, value);
   return ref;
 };
 
@@ -112,8 +112,8 @@ op.atposref_n = function(hllName, obj, index) {
   }
   var STable = refType._STable;
   var ref = STable.REPR.allocate(STable);
-  ref.get = function() {return obj.$$atpos(index)};
-  ref.set = function(value) {return obj.$$bindpos(index, value)};
+  ref.get = () => obj.$$atpos(index);
+  ref.set = value => obj.$$bindpos(index, value);
   return ref;
 };
 
@@ -124,8 +124,8 @@ op.atposref_s = function(hllName, obj, index) {
   }
   var STable = refType._STable;
   var ref = STable.REPR.allocate(STable);
-  ref.get = function() {return obj.$$atpos(index)};
-  ref.set = function(value) {return obj.$$bindpos(index, value)};
+  ref.get = () => obj.$$atpos(index);
+  ref.set = value => obj.$$bindpos(index, value);
   return ref;
 };
 
