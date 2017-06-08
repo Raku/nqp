c9ca5de8dc3d986c5e2b7dd71eb1db99c8863adb
Jonathan Worthington <jnthn@jnthn.net>
2017-06-08 12:57:30 +0200

    Start async process, conveying any errors.

--- src/vm/jvm/runtime/org/perl6/nqp/io/AsyncProcessHandle.java
+++ src/vm/jvm/runtime/org/perl6/nqp/io/AsyncProcessHandle.java
@@ -1,5 +1,8 @@
 package org.perl6.nqp.io;
 
+import java.io.File;
+import java.lang.ProcessBuilder;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,13 +19,47 @@ import org.perl6.nqp.sixmodel.reprs.ConcBlockingQueueInstance;
 import org.perl6.nqp.sixmodel.reprs.IOHandleInstance;
 
 public class AsyncProcessHandle implements IIOClosable {
+    private Process proc;
+    private SixModelObject queue;
+    private ThreadContext tc;
+    private HLLConfig hllConfig;
+
     public AsyncProcessHandle(ThreadContext tc, SixModelObject queue, SixModelObject argsObj,
             String cwd, SixModelObject envObj, SixModelObject configObj) {
         List<String> args = getArgs(tc, argsObj);
         Map<String, String> env = getEnv(tc, envObj);
         Map<String, SixModelObject> config = getConfig(tc, configObj);
 
-        throw ExceptionHandling.dieInternal(tc, "NYI");
+        ProcessBuilder pb = new ProcessBuilder(args);
+        pb.directory(new File(cwd));
+        pb.environment().clear();
+        pb.environment().putAll(env);
+        setupInputOutput(tc, pb, config);
+
+        this.queue = queue;
+        this.tc = tc;
+        this.hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
+        try {
+            this.proc = pb.start();
+            SixModelObject ready = config.get("ready");
+            if (ready != null)
+                send(ready);
+        }
+        catch (Throwable t) {
+            SixModelObject message = boxError(t.getMessage());
+
+            SixModelObject error = config.get("error");
+            if (error != null)
+                send(error, message);
+
+            SixModelObject stdoutBytes = config.get("stdout_bytes");
+            if (stdoutBytes != null)
+                send(stdoutBytes, this.hllConfig.intBoxType, this.hllConfig.strBoxType, message);
+
+            SixModelObject stderrBytes = config.get("stderr_bytes");
+            if (stderrBytes != null)
+                send(stderrBytes, this.hllConfig.intBoxType, this.hllConfig.strBoxType, message);
+        }
     }
 
     private List<String> getArgs(ThreadContext tc, SixModelObject argsObj) {
@@ -56,6 +93,23 @@ public class AsyncProcessHandle implements IIOClosable {
             env.put(Ops.iterkey_s(kv, tc), Ops.iterval(kv, tc));
         }
         return env;
+    }
+
+    private void setupInputOutput(ThreadContext tc, ProcessBuilder pb,
+            Map<String, SixModelObject> config) {
+        // TODO Implement this
+    }
+
+    private void send(SixModelObject... args) {
+        final SixModelObject Array = this.hllConfig.listType;
+        SixModelObject result = Array.st.REPR.allocate(this.tc, Array.st);
+        for (SixModelObject arg : args)
+            result.push_boxed(this.tc, arg);
+        ((ConcBlockingQueueInstance)this.queue).push_boxed(this.tc, result);
+    }
+
+    private SixModelObject boxError(String error) {
+        return Ops.box_s(error, this.hllConfig.strBoxType, this.tc);
     }
 
     public void writeBytes(ThreadContext tc, AsyncTaskInstance task, SixModelObject toWrite) {
