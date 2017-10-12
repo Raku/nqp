31a40557c2c9b357e1976396c89cc458f57ed826
usev6 <use_v6@aglaz.de>
2017-10-12 07:58:18 +0200

    [jvm] Avoid failure in getattr
    An exception was raised in getattr, because we tried to
    set the first field found (which was 'private static final').
    
    A better fix might be to change the logic in getattr, but
    this helps for now.

--- src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/P6bigintInstance.java
+++ src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/P6bigintInstance.java
@@ -7,13 +7,14 @@ import org.perl6.nqp.sixmodel.SixModelObject;
 import org.perl6.nqp.runtime.ExceptionHandling;
 
 public class P6bigintInstance extends SixModelObject {
-    private static final BigInteger SMALLEST_UNBOXABLE = new BigInteger(String.valueOf(Long.MIN_VALUE));
     public BigInteger value;
-    
+
+    private static final BigInteger SMALLEST_UNBOXABLE = new BigInteger(String.valueOf(Long.MIN_VALUE));
+
     public void set_int(ThreadContext tc, long value) {
         this.value = BigInteger.valueOf(value);
     }
-    
+
     public long get_int(ThreadContext tc) {
         if (value.bitLength() >= 64 && !equals(SMALLEST_UNBOXABLE)) {
             throw ExceptionHandling.dieInternal(tc, "Cannot unbox " + value.bitLength() + " bit wide bigint into native integer");
