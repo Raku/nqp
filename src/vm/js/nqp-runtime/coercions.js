119826e200e5c14a60322826e3463d1c764da387
pmurias <pawelmurias@gmail.com>
2017-10-26 10:20:07 +0200

    [js] Coerce num to strings more like MoarVM does

--- src/vm/js/nqp-runtime/coercions.js
+++ src/vm/js/nqp-runtime/coercions.js
@@ -16,7 +16,7 @@ exports.numToStr = function(num) {
   if (num === -Infinity) return '-Inf';
   if (num === 0 && 1/num === -Infinity) return '-0';
 
-  return num.toString();
+  return num.toPrecision(15).replace(/\.?0+$/, '');
 };
 
 /* TODO - more error checking */
