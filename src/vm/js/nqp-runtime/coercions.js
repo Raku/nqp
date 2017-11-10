3017f69e18b1363edabbada6eee5be511737fd7d
pmurias <pawelmurias@gmail.com>
2017-11-10 13:04:57 +0100

    [js] Fix floating point stringification

--- src/vm/js/nqp-runtime/coercions.js
+++ src/vm/js/nqp-runtime/coercions.js
@@ -16,7 +16,8 @@ exports.numToStr = function(num) {
   if (num === -Infinity) return '-Inf';
   if (num === 0 && 1/num === -Infinity) return '-0';
 
-  return num.toPrecision(15).replace(/\.?0+$/, '');
+  const parts = num.toPrecision(15).split(/e/);
+  return parts[0].replace(/\.?0+$/, '') + (parts.length === 1 ? '' : 'e' + parts[1]);
 };
 
 /* TODO - more error checking */
