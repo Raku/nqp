09556605ff249a0b2da8c8922aaa107acf2660ce
pmurias <pawelmurias@gmail.com>
2018-02-15 13:19:38 +0100

    [js] Remove polyfill

--- src/vm/js/nqp-runtime/runtime.js
+++ src/vm/js/nqp-runtime/runtime.js
@@ -260,22 +260,6 @@ exports.toInt = function(arg, ctx) {
   return (exports.toNum(arg, ctx) | 0);
 };
 
-
-if (!Math.imul) {
-  /* Polyfill from:
-  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/imul
-  */
-  Math.imul = function(a, b) {
-    const ah = (a >>> 16) & 0xffff;
-    const al = a & 0xffff;
-    const bh = (b >>> 16) & 0xffff;
-    const bl = b & 0xffff;
-    // the shift by 0 fixes the sign on the high part
-    // the final |0 converts the unsigned value into a signed value
-    return ((al * bl) + (((ah * bl + al * bh) << 16) >>> 0) | 0);
-  };
-}
-
 // Placeholder
 exports.topContext = function() {
   return null;
