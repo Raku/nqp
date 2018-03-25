be7fd36188773f163ffd75255ea5604d0526aef2
Stefan Seifert <nine@detonation.org>
2018-03-25 13:18:13 +0200

    Make NQP's source digest available through the compiler config
    This gives NQP's user precise information about the NQP version used and makes
    it possible to detect any change in NQP's sources.

--- tools/build/gen-version.pl
+++ tools/build/gen-version.pl
@@ -6,6 +6,8 @@ gen-version.pl -- script to generate version information for HLL compilers
 
 =cut
 
+use Digest::SHA;
+use File::Find;
 use POSIX 'strftime';
 
 my $prefix = shift;
@@ -25,12 +27,17 @@ chomp $VERSION;
 
 my $builddate = strftime('%Y-%m-%dT%H:%M:%SZ', gmtime);
 
+my $sha = Digest::SHA->new;
+find(sub { next unless /\.nqp\z/; $sha->addfile($_) }, "src");
+my $source_digest = $sha->hexdigest;
+
 print <<"END_VERSION";
 sub hll-config(\$config) {
-    \$config<version>    := '$VERSION';
-    \$config<build-date> := '$builddate';
-    \$config<prefix>     := '$prefix';
-    \$config<libdir>     := '$libdir';
+    \$config<version>       := '$VERSION';
+    \$config<build-date>    := '$builddate';
+    \$config<prefix>        := '$prefix';
+    \$config<libdir>        := '$libdir';
+    \$config<source-digest> := '$source_digest';
 }
 END_VERSION
 
