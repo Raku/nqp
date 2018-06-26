cc2990d07bf08ca8d40f3127ae7f853c999d930c
Paweł Murias <pawelmurias@gmail.com>
2018-06-26 21:50:58 +0200

    [truffle] Add make t-test target, passes 5 tests

---
+++ src/vm/jvm/bin/run_tests.pl
@@ -0,0 +1,14 @@
+use TAP::Harness;
+
+my ($skip_qregex) = @ARGV;
+
+my $harness = TAP::Harness->new({
+    exec => sub {
+        my ($harness, $test_file) = @_;
+        return ['./nqp-j', 'nqp-truffle.nqp', $test_file] if $test_file =~ /\.t$/;
+    }
+});
+
+my @nqp_tests = glob "t/nqp/{001,002,003,004,005}*.t";
+
+$harness->runtests(@nqp_tests);
