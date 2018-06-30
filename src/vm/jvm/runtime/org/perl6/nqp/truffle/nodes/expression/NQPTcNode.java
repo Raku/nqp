c79cfa75eb5b59c6683d12ccf7581e5eb5a46c90
Daniel Green <ddgreen@gmail.com>
2018-06-30 08:13:31 -0400

    [truffle] Implement nqp::tc

---
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPTcNode.java
@@ -0,0 +1,32 @@
+package org.perl6.nqp.truffle.nodes.expression;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import org.perl6.nqp.truffle.nodes.NQPNode;
+import org.perl6.nqp.dsl.Deserializer;
+
+@NodeInfo(shortName = "tc")
+public final class NQPTcNode extends NQPNode {
+    @Child private NQPNode argNode;
+
+    @Deserializer
+    public NQPTcNode(NQPNode argNode) {
+        this.argNode = argNode;
+    }
+
+    static String codepointToTitleCase(int codepoint) {
+        if (codepoint == 223) return "Ss";
+        return new String(Character.toChars(Character.toTitleCase(codepoint)));
+    }
+
+    @Override
+    public String executeStr(VirtualFrame frame) {
+        String val = argNode.executeStr(frame);
+        String ret = "";
+        for (int offset = 0; offset < val.length(); ) {
+            int codepoint = val.codePointAt(offset);
+            ret += codepointToTitleCase(codepoint);
+            offset += Character.charCount(codepoint);
+        }
+        return ret;
+    }
+}
