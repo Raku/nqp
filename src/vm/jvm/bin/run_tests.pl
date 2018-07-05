547dafe72e0bfa755092c55134e0c9916b49dba4
Paweł Murias <pawelmurias@gmail.com>
2018-07-05 18:02:22 +0200

    [truffle] Run passing test 36

--- src/vm/jvm/bin/run_tests.pl
+++ src/vm/jvm/bin/run_tests.pl
@@ -9,6 +9,6 @@ my $harness = TAP::Harness->new({
     }
 });
 
-my @nqp_tests = glob "t/nqp/{001,002,003,004,005,008,010,013,016,083}*.t";
+my @nqp_tests = glob "t/nqp/{001,002,003,004,005,008,010,013,016,036,083}*.t";
 
 $harness->runtests(@nqp_tests);
