9f21e57ee2b5af9395b360cf8a5738dee77e1870
Daniel Green <ddgreen@gmail.com>
2018-07-14 17:16:20 -0400

    [truffle] Correct the class these extend

--- src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPLstat_timeNode.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPLstat_timeNode.java
@@ -2,13 +2,13 @@ package org.perl6.nqp.truffle.nodes.expression;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import org.perl6.nqp.truffle.nodes.NQPNode;
-import org.perl6.nqp.truffle.nodes.NQPIntNode;
+import org.perl6.nqp.truffle.nodes.NQPNumNode;
 import org.perl6.nqp.truffle.runtime.FileOps;
 import org.perl6.nqp.dsl.Deserializer;
 import java.nio.file.LinkOption;
 
 @NodeInfo(shortName = "lstat_time")
-public final class NQPLstat_timeNode extends NQPIntNode {
+public final class NQPLstat_timeNode extends NQPNumNode {
     @Child private NQPNode leftNode;
     @Child private NQPNode rightNode;
 
--- src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPStat_timeNode.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPStat_timeNode.java
@@ -2,12 +2,12 @@ package org.perl6.nqp.truffle.nodes.expression;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import org.perl6.nqp.truffle.nodes.NQPNode;
-import org.perl6.nqp.truffle.nodes.NQPIntNode;
+import org.perl6.nqp.truffle.nodes.NQPNumNode;
 import org.perl6.nqp.truffle.runtime.FileOps;
 import org.perl6.nqp.dsl.Deserializer;
 
 @NodeInfo(shortName = "stat_time")
-public final class NQPStat_timeNode extends NQPIntNode {
+public final class NQPStat_timeNode extends NQPNumNode {
     @Child private NQPNode leftNode;
     @Child private NQPNode rightNode;
 
