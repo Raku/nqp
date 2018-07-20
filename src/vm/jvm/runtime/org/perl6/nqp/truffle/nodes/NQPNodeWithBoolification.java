97355496be28d964170a8c7f20649423eafbba32
Paweł Murias <pawelmurias@gmail.com>
2018-07-20 17:12:15 +0200

    [truffle] Boolification of nqp::null

--- src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/NQPNodeWithBoolification.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/NQPNodeWithBoolification.java
@@ -3,6 +3,7 @@ package org.perl6.nqp.truffle.nodes;
 
 import org.perl6.nqp.truffle.nodes.NQPNode;
 import org.perl6.nqp.truffle.runtime.NQPListIterator;
+import org.perl6.nqp.truffle.runtime.NQPNull;
 
 public abstract class NQPNodeWithBoolification extends NQPNode {
     /* TODO - proper checking */
@@ -15,6 +16,8 @@ public abstract class NQPNodeWithBoolification extends NQPNode {
             return ((double) arg) != 0 ? true : false;
         } else if (arg instanceof NQPListIterator) {
             return ((NQPListIterator) arg).boolify();
+        } else if (arg == NQPNull.SINGLETON) {
+            return false;
         } else {
             throw new RuntimeException("Can't boolify");
         }
