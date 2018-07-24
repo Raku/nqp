b64648d8735d54cdbb0fb24875624e50f13a61a3
Paweł Murias <pawelmurias@gmail.com>
2018-07-24 12:19:50 +0200

    [truffle] Use @Specialization when boolifing stuff

---
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/NQPToBooleanNode.java
@@ -0,0 +1,38 @@
+package org.perl6.nqp.truffle.nodes;
+
+import com.oracle.truffle.api.dsl.Specialization;
+import org.perl6.nqp.truffle.runtime.NQPListIterator;
+import org.perl6.nqp.truffle.runtime.NQPNull;
+
+public abstract class NQPToBooleanNode extends NQPBaseNode {
+    public abstract boolean executeBoolean(Object value);
+
+    @Specialization
+    protected boolean doString(String value) {
+        return ((String) value != "") ? true : false;
+    }
+
+    @Specialization
+    protected boolean doLong(Long value) {
+        return ((long) value) != 0 ? true : false;
+    }
+
+    @Specialization
+    protected boolean doDouble(Double value) {
+        return ((double) value) != 0 ? true : false;
+    }
+
+    @Specialization
+    protected boolean doNQPListIterator(NQPListIterator value) {
+        return ((NQPListIterator) value).boolify();
+    }
+
+    @Specialization
+    protected boolean doNQPNull(Object value) {
+        return false;
+    }
+
+    protected final boolean isNull(Object value) {
+        return value == NQPNull.SINGLETON;
+    }
+}
