0817412c165d1b91f0b07f6c1daf914e37727211
Paweł Murias <pawelmurias@gmail.com>
2018-07-24 12:32:50 +0200

    [truffle] Missing part of moving boolification to specialization

--- src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/NQPNodeWithBoolification.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/NQPNodeWithBoolification.java
@@ -1,25 +1,16 @@
 package org.perl6.nqp.truffle.nodes;
 
-
 import org.perl6.nqp.truffle.nodes.NQPNode;
-import org.perl6.nqp.truffle.runtime.NQPListIterator;
-import org.perl6.nqp.truffle.runtime.NQPNull;
+import com.oracle.truffle.api.CompilerDirectives;
 
 public abstract class NQPNodeWithBoolification extends NQPNode {
-    /* TODO - proper checking */
-    protected boolean toBoolean(Object arg) {
-        if (arg instanceof String) {
-            return ((String) arg != "") ? true : false;
-        } else if (arg instanceof Long) {
-            return ((long) arg) != 0 ? true : false;
-        } else if (arg instanceof Double) {
-            return ((double) arg) != 0 ? true : false;
-        } else if (arg instanceof NQPListIterator) {
-            return ((NQPListIterator) arg).boolify();
-        } else if (arg == NQPNull.SINGLETON) {
-            return false;
-        } else {
-            throw new RuntimeException("Can't boolify");
+    @Child private NQPToBooleanNode toBooleanCast;
+
+    protected boolean toBoolean(Object value) {
+        if (toBooleanCast == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            toBooleanCast = insert(NQPToBooleanNodeGen.create());
         }
+        return toBooleanCast.executeBoolean(value);
     }
 }
