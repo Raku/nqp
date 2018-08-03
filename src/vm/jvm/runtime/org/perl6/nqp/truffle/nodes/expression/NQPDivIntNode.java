10bed0641b15ec46ffbaeadbf797aad21013d750
Paweł Murias <pawelmurias@gmail.com>
2018-08-03 16:37:48 +0200

    [truffle] Fix nqp::div_i

--- src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPDivIntNode.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPDivIntNode.java
@@ -7,17 +7,23 @@ import org.perl6.nqp.dsl.Deserializer;
 
 @NodeInfo(shortName = "div_i")
 public final class NQPDivIntNode extends NQPIntNode {
-    @Child private NQPNode leftNode;
-    @Child private NQPNode rightNode;
+    @Child private NQPNode dividentNode;
+    @Child private NQPNode divisorNode;
 
     @Deserializer
-    public NQPDivIntNode(NQPNode leftNode, NQPNode rightNode) {
-        this.leftNode = leftNode;
-        this.rightNode = rightNode;
+    public NQPDivIntNode(NQPNode dividentNode, NQPNode divisorNode) {
+        this.dividentNode = dividentNode;
+        this.divisorNode = divisorNode;
     }
 
     @Override
     public long executeInt(VirtualFrame frame) {
-        return leftNode.executeInt(frame) / rightNode.executeInt(frame);
+        long divident = dividentNode.executeInt(frame);
+        long divisor = divisorNode.executeInt(frame);
+        if (divisor == 0) {
+            //TODO: throw an exception
+            //die_s("Division by zero", tc);
+        }
+        return (long)Math.floor((double) divident / divisor);
     }
 }
