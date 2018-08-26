792c37849b69a9563bd4c320363b4a325b78311f
Paweł Murias <pawelmurias@gmail.com>
2018-08-26 10:56:41 +0200

    [js] Tweak in browser loading of code

--- src/vm/js/nqp-runtime/runtime.js
+++ src/vm/js/nqp-runtime/runtime.js
@@ -137,9 +137,13 @@ exports.loaderCtx = null;
 
 if (process.browser) {
   op.loadbytecode = /*async*/ function(ctx, file) {
+      if (file == '/nqp/lib/Perl6/BOOTSTRAP.js') {
+        file = 'Perl6::BOOTSTRAP';
+      }
+
       const oldLoaderCtx = exports.loaderCtx;
       exports.loaderCtx = ctx;
-      file = file.replace(/\.setting$/, '_setting');
+      file = file.replace(/\.setting(\.js)?$/, '_setting');
       file = file.replace(/::/g, '-');
       require('./' + file + '.nqp-raw-runtime');
       exports.loaderCtx = oldLoaderCtx;
