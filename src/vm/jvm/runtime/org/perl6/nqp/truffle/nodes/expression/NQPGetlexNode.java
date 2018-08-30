321c790756726fbbba246e05866d434ef9c88e32
Daniel Green <ddgreen@gmail.com>
2018-08-30 12:23:36 -0400

    [truffle] Implement nqp::getlex and _<i n s>

---
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPGetlexNode.java
@@ -0,0 +1,25 @@
+package org.perl6.nqp.truffle.nodes.expression;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import org.perl6.nqp.runtime.CallFrame;
+import org.perl6.nqp.sixmodel.SixModelObject;
+import org.perl6.nqp.truffle.nodes.NQPNode;
+import org.perl6.nqp.truffle.nodes.NQPObjNode;
+import org.perl6.nqp.dsl.Deserializer;
+
+@NodeInfo(shortName = "getlex_o")
+public final class NQPGetlexObjNode extends NQPObjNode {
+    @Child private NQPNode leftNode;
+    @Child private NQPNode rightNode;
+
+    @Deserializer
+    public NQPGetlexObjNode(NQPNode leftNode, NQPNode rightNode) {
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+
+    @Override
+    public SixModelObject execute(VirtualFrame frame) {
+        return ((CallFrame)leftNode.execute(frame)).oLex[(int)rightNode.executeInt(frame)];
+    }
+}
