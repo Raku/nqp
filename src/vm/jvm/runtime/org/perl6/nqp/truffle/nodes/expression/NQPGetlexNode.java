85c90e8cad9361e4c3e3d06139d88c3702ebf03c
Paweł Murias <pawelmurias@gmail.com>
2018-08-31 14:27:58 +0200

    Revert "[truffle] Fix wrong class name in getlex"
    This reverts commit ed653e198a753a2e3700eb0b98f5135063f0300d.

--- src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPGetlexNode.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPGetlexNode.java
@@ -8,12 +8,12 @@ import org.perl6.nqp.truffle.nodes.NQPObjNode;
 import org.perl6.nqp.dsl.Deserializer;
 
 @NodeInfo(shortName = "getlex_o")
-public final class NQPGetlexNode extends NQPObjNode {
+public final class NQPGetlexObjNode extends NQPObjNode {
     @Child private NQPNode leftNode;
     @Child private NQPNode rightNode;
 
     @Deserializer
-    public NQPGetlexNode(NQPNode leftNode, NQPNode rightNode) {
+    public NQPGetlexObjNode(NQPNode leftNode, NQPNode rightNode) {
         this.leftNode = leftNode;
         this.rightNode = rightNode;
     }
