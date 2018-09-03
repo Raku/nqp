9d73c4138e9228c6f170461f0baa060f0337b07d
Paweł Murias <pawelmurias@gmail.com>
2018-09-03 12:20:10 +0200

    [truffle] Make nqp::getpid work without using reflection

--- src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPGetpidNode.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/nodes/expression/NQPGetpidNode.java
@@ -1,11 +1,11 @@
 package org.perl6.nqp.truffle.nodes.expression;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import org.perl6.nqp.truffle.nodes.NQPNode;
 import org.perl6.nqp.truffle.nodes.NQPIntNode;
 import org.perl6.nqp.dsl.Deserializer;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.management.ManagementFactory;
 
 @NodeInfo(shortName = "getpid")
 public final class NQPGetpidNode extends NQPIntNode {
@@ -16,20 +16,21 @@ public final class NQPGetpidNode extends NQPIntNode {
 
     @Override
     public long executeInt(VirtualFrame frame) {
-        /* Questionably portable; see:
-         * http://boxysystems.com/index.php/java-tip-find-process-id-of-running-java-process/
-         */
-        try {
-            java.lang.management.RuntimeMXBean runtime = java.lang.management.ManagementFactory.getRuntimeMXBean();
-            Field jvm = runtime.getClass().getDeclaredField("jvm");
-            jvm.setAccessible(true);
-            Object mgmt = jvm.get(runtime);
-            Method pid_method = mgmt.getClass().getDeclaredMethod("getProcessId");
-            pid_method.setAccessible(true);
-            return (Integer)pid_method.invoke(mgmt);
+        return getPid();
+    }
+
+    // We are doing the parsing because GraalVM isn't using JDK 11 yet
+    @TruffleBoundary
+    public long getPid() {
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        int offset = name.indexOf('@');
+        if (offset == -1) {
+            throw new RuntimeException("Can't get pid");;
         }
-        catch (Throwable t) {
-            throw new RuntimeException(t);
+        try {
+            return Long.decode(name.substring(0, offset));
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Can't get pid");
         }
     }
 }
