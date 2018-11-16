82930f58d3f3460a08e58487beef3abc31fce2a9
Paweł Murias <pawelmurias@gmail.com>
2018-11-16 16:31:58 +0100

    [js] Fixup our asIntN, asUintN versions

--- src/vm/js/nqp-runtime/as-int-n.js
+++ src/vm/js/nqp-runtime/as-int-n.js
@@ -1,10 +1,10 @@
 const JSBI = require('jsbi');
 
 module.exports.asIntN = function(bits, bigint) {
-  return JSBI.BigInt(BigInt.asIntN(bits, BigInt(bigint.toString())));
+  return JSBI.BigInt(BigInt.asIntN(bits, BigInt(bigint.toString())).toString());
 };
 
 module.exports.asUintN = function(bits, bigint) {
-  return JSBI.BigInt(BigInt.asUIntN(bits, BigInt(bigint.toString())));
+  return JSBI.BigInt(BigInt.asUintN(bits, BigInt(bigint.toString())).toString());
 };
 
