5c1e327167570d3053977534e7e26fa281ced668
Paweł Murias <pawelmurias@gmail.com>
2018-11-16 16:38:15 +0100

    [js] Use naive directly from the spec asIntN and asUIntN
    That avoid using BigInt and will be replaced once efficent versions are in jsbi

--- src/vm/js/nqp-runtime/as-int-n.js
+++ src/vm/js/nqp-runtime/as-int-n.js
@@ -1,10 +1,12 @@
 const JSBI = require('jsbi');
 
 module.exports.asIntN = function(bits, bigint) {
-  return JSBI.BigInt(BigInt.asIntN(bits, BigInt(bigint.toString())).toString());
+  const power = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(bits));
+  const mod = JSBI.remainder(bigint, power);
+  return (JSBI.greaterThanOrEqual(mod, JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(bits-1))) ? JSBI.subtract(mod, power) : mod);
 };
 
 module.exports.asUintN = function(bits, bigint) {
-  return JSBI.BigInt(BigInt.asUintN(bits, BigInt(bigint.toString())).toString());
+  return JSBI.remainder(bigint, JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(bits)));
 };
 
