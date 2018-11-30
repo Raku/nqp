8abeb77bc633f9908643eb6eb28af986581af7ad
Paweł Murias <pawelmurias@gmail.com>
2018-11-30 12:59:40 +0100

    [js] Move exceptionsStack to globalContext

--- src/vm/js/nqp-runtime/exceptions-stack.js
+++ src/vm/js/nqp-runtime/exceptions-stack.js
@@ -1,8 +1,12 @@
 const fibers = process.browser ? {} : require('fibers');
-const globalStack = [];
+
+const globalContext = require('./global-context.js');
+
+globalContext.initialize(context => context.exceptionsStack = []);
+
 module.exports = function() {
   if (fibers.current === undefined) {
-    return globalStack;
+    return globalContext.context.exceptionsStack;
   } else if (fibers.current) {
     if (fibers.current.exceptionsStack === undefined) {
       fibers.current.exceptionsStack = [];
