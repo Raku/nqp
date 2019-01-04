36ddbe494170955e7996e6c186f11b4ca78a61c7
Paweł Murias <pawelmurias@gmail.com>
2019-01-04 15:02:44 +0100

    [js] Fix our asIntN "polyfill"

--- src/vm/js/nqp-runtime/as-int-n.js
+++ src/vm/js/nqp-runtime/as-int-n.js
@@ -1,12 +1,25 @@
 const JSBI = require('jsbi');
 
+/* as defined in the ECMA-262 spec */
+function modulo(bigint, power) {
+  const mod = JSBI.remainder(bigint, power);
+
+  if (JSBI.lessThan(mod, JSBI.BigInt(0))) {
+    return JSBI.add(mod, power);
+  } else {
+    return mod;
+  }
+}
+
 module.exports.asIntN = function(bits, bigint) {
   const power = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(bits));
-  const mod = JSBI.remainder(bigint, power);
+
+  const mod = modulo(bigint, power);
+
   return (JSBI.greaterThanOrEqual(mod, JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(bits-1))) ? JSBI.subtract(mod, power) : mod);
 };
 
 module.exports.asUintN = function(bits, bigint) {
-  return JSBI.remainder(bigint, JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(bits)));
+  return modulo(bigint, JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(bits)));
 };
 
