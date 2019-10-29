1bba032683f60daa99d0d55d6f8d8b3ac6ae27b6
Daniel Green <ddgreen@gmail.com>
2019-10-28 20:06:13 -0400

    [truffle] Fixes to build nqp-truffle-impl.jar

--- src/vm/jvm/runtime/org/perl6/nqp/truffle/svm/Truffle6.java
+++ src/vm/jvm/runtime/org/perl6/nqp/truffle/svm/Truffle6.java
@@ -45,6 +45,7 @@ import java.io.PrintStream;
 import org.perl6.nqp.truffle.ByteCodeRunnerGen;
 import org.perl6.nqp.truffle.GlobalContext;
 import org.perl6.nqp.truffle.ThreadContext;
+import org.perl6.nqp.truffle.runtime.DynamicContext;
 
 public class Truffle6 {
     public static void help(PrintStream stream) {
@@ -60,7 +61,7 @@ public class Truffle6 {
         } else if (args[0] == "--help") {
             help(System.out);
         } else {
-            (new ByteCodeRunnerGen()).runByteCode(new GlobalContext(), new ThreadContext(), args[0]);
+            (new ByteCodeRunnerGen()).runByteCode(new GlobalContext(), new ThreadContext(), new DynamicContext(), args[0]);
         }
     }
 }
