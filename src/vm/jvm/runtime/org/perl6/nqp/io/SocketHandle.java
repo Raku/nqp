954854652e565333dc757ee028a091144d843a96
Ben Davies <kaiepi@outlook.com>
2020-02-14 23:12:13 -0400

    [JVM] Implement support for bind before connect with sync sockets

--- src/vm/jvm/runtime/org/perl6/nqp/io/SocketHandle.java
+++ src/vm/jvm/runtime/org/perl6/nqp/io/SocketHandle.java
@@ -8,7 +8,7 @@ import java.nio.charset.Charset;
 import org.perl6.nqp.runtime.ExceptionHandling;
 import org.perl6.nqp.runtime.ThreadContext;
 
-public class SocketHandle extends SyncHandle {
+public class SocketHandle extends SyncHandle implements IIOBindable {
 
     public SocketHandle(ThreadContext tc) {
         try {
@@ -22,6 +22,16 @@ public class SocketHandle extends SyncHandle {
     public SocketHandle(ThreadContext tc, SocketChannel existing) {
         chan = existing;
         setEncoding(tc, Charset.forName("UTF-8"));
+    }
+
+    @Override
+    public void bind(ThreadContext tc, String host, int port) {
+        try {
+            InetSocketAddress addr = new InetSocketAddress(host, port);
+            ((SocketChannel)chan).bind(addr);
+        } catch (IOException e) {
+            throw ExceptionHandling.dieInternal(tc, e);
+        }
     }
 
     public void connect(ThreadContext tc, String host, int port) {
