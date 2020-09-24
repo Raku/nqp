fc058f84ee1b23d5d09b025fb7b6f2a6e164babf
Christian Bartolomäus <use_v6@aglaz.de>
2020-09-24 21:16:50 +0200

    [JVM] Use singleton object for vmnull

--- src/vm/jvm/runtime/org/raku/nqp/sixmodel/reprs/VMNullInstance.java
+++ src/vm/jvm/runtime/org/raku/nqp/sixmodel/reprs/VMNullInstance.java
@@ -1,6 +1,25 @@
 package org.raku.nqp.sixmodel.reprs;
 
+import org.raku.nqp.runtime.ThreadContext;
 import org.raku.nqp.sixmodel.SixModelObject;
 
 public class VMNullInstance extends SixModelObject {
+    // singleton object
+    private static volatile VMNullInstance theVMNull;
+
+    private VMNullInstance(ThreadContext tc) {
+        this.st = tc.gc.VMNull.st;
+    }
+
+    public synchronized static VMNullInstance getInstance(ThreadContext tc) {
+        // double check to make singleton thread safe
+        if (theVMNull == null) {
+            synchronized (VMNullInstance.class) {
+                if (theVMNull == null)
+                    theVMNull = new VMNullInstance(tc);
+            }
+        }
+
+        return theVMNull;
+    }
 }
