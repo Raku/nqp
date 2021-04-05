b7c0ae276b7820cec20a3270674e893a05c13eb0
Christian Bartolomäus <use_v6@aglaz.de>
2021-04-05 12:41:58 +0200

    [JVM] Make nqp::unbox_i on NativeCall object work
    It is supposed to report whether the library was loaded or not. This
    is a port of https://github.com/MoarVM/MoarVM/commit/1b0950e3e9 and
    is needed since https://github.com/rakudo/rakudo/commit/9ef7a7e9dd.
    
    nine++ gave the hint for the concrete implementation on the JVM.
    
    Please note, that the usage of nqp::unbox_i($!call) in
    lib/NativeCall.rakudmod don't work yet. They result in a
    NullPointerException -- but that's probably a different bug. At least
    the following code works now:
    
      use nqp;
      use NativeCall;
      my class native_callsite is repr('NativeCall') { };
      my $bar = native_callsite.new;
      say nqp::unbox_i($bar);  ## 0

--- src/vm/jvm/runtime/org/raku/nqp/sixmodel/reprs/NativeCallInstance.java
+++ src/vm/jvm/runtime/org/raku/nqp/sixmodel/reprs/NativeCallInstance.java
@@ -1,5 +1,6 @@
 package org.raku.nqp.sixmodel.reprs;
 
+import org.raku.nqp.runtime.ThreadContext;
 import org.raku.nqp.sixmodel.SixModelObject;
 
 public class NativeCallInstance extends SixModelObject {
@@ -7,4 +8,9 @@ public class NativeCallInstance extends SixModelObject {
      * P6opaque; this is about the best we can do on the JVM, which doesn't
      * support interior pointers of complex value types. */
     public NativeCallBody body;
+
+    @Override
+    public long get_int(ThreadContext tc) {
+        return body == null || body.entry_point == null ? 0 : 1;
+    }
 }
