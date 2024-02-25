d93416692430cf69c6bb262b4cddefa7cd71f53e
Daniel Green <ddgreen@gmail.com>
2024-02-24 22:29:03 -0500

    Convert cpNameMap to use fastutil collection

--- src/vm/jvm/runtime/org/raku/nqp/runtime/Ops.java
+++ src/vm/jvm/runtime/org/raku/nqp/runtime/Ops.java
@@ -66,6 +66,8 @@ import java.util.TimerTask;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+
 import org.raku.nqp.io.FileHandle;
 import org.raku.nqp.io.IIOBindable;
 import org.raku.nqp.io.IIOCancelable;
@@ -4464,43 +4466,43 @@ public final class Ops {
     }
 
     /* Brute force, but not normally needed for most programs. */
-    private static volatile HashMap<String, Integer> cpNameMap;
+    private static volatile Object2IntOpenHashMap cpNameMap;
     private static volatile Boolean cpNameMapAboveBMP;
     public static long codepointfromname(String name) {
-        HashMap<String, Integer> names = cpNameMap;
+        Object2IntOpenHashMap names = cpNameMap;
         /* Gets the first half (the BMP) */
         if (names == null) {
             /* Initialize the expected max hash size as 0x10FFFF */
-            names = new HashMap< >(0x10FFFF);
+            names = new Object2IntOpenHashMap(0x10FFFF);
             for (int i = 0; i < Character.MAX_VALUE; i++)
                 if (Character.isValidCodePoint(i))
                     names.put(Character.getName(i), i);
-            names.put("ALERT",            (int)7);
-            names.put("BEL",              (int)7);
-            names.put("LF",               (int)10);
-            names.put("LINE FEED",        (int)10);
-            names.put("FF",               (int)12);
-            names.put("FORM FEED",        (int)12);
-            names.put("CR",               (int)13);
-            names.put("CARRIAGE RETURN",  (int)13);
-            names.put("NEL",              (int)133);
-            names.put("NEXT LINE",        (int)133);
+            names.put("ALERT",            7);
+            names.put("BEL",              7);
+            names.put("LF",               10);
+            names.put("LINE FEED",        10);
+            names.put("FF",               12);
+            names.put("FORM FEED",        12);
+            names.put("CR",               13);
+            names.put("CARRIAGE RETURN",  13);
+            names.put("NEL",              133);
+            names.put("NEXT LINE",        133);
             names.remove("BELL"); // added below as 0x1F514, cmp. RT #130542
             cpNameMap = names;
             cpNameMapAboveBMP = false;
         }
-        Integer found = names.get(name);
+        int found = names.getOrDefault(name, -1);
         /* If we have not found it yet, put all other possible codepoints into
          * the hash */
-        if (found == null && !cpNameMapAboveBMP) {
+        if (found == -1 && !cpNameMapAboveBMP) {
             for (int i = Character.MAX_VALUE; i <= 0x10FFFF; i++)
                 if (Character.isValidCodePoint(i))
                     names.put(Character.getName(i), i);
-            names.put("BELL",             (int)0x1F514);
+            names.put("BELL",             0x1F514);
             cpNameMapAboveBMP = true;
-            found = names.get(name);
+            found = names.getOrDefault(name, -1);
         }
-        return found == null ? -1 : found;
+        return found;
     }
 
     public static String strfromname(String name) {
